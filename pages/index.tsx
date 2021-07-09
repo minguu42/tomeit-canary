6c5f344655909f8c7934809540c9cd54d3bb7842
minguu <minguu42@gmail.com>
2021-07-09 21:18:38 +0900

    Document コンポーネントの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/logo512.png" alt="tomeit Logo" width={72} height={72} />
           </span>
         </a>
       </footer>
