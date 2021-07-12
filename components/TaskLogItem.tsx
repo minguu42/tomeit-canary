53a112ff6c294d2a0b53ddd083524b2cd13a4dc1
minguu <minguu42@gmail.com>
2021-07-12 14:24:46 +0900

    logs ページの実装

---
+++ components/TaskLogItem.tsx
@@ -0,0 +1,29 @@
+import { createContext, VFC } from "react";
+
+import styles from "styles/components/TaskLogItem.module.scss";
+import TimerIcon from "components/icons/TimerIcon";
+import { convertDatetimeToTime } from "lib/format";
+
+export type TaskLog = {
+  id: number;
+  name: string;
+  pomodoroCount: number;
+  completeAt: string;
+};
+
+type Props = TaskLog;
+
+const TaskLogItem: VFC<Props> = ({ name, pomodoroCount, completeAt }) => (
+  <li className={styles.outer}>
+    <div className={styles.leftWrapper}>
+      <div className={styles.countWrapper}>
+        <TimerIcon fill="#192f60" />
+        <p>{pomodoroCount}</p>
+      </div>
+      <p className={styles.name}>{name}</p>
+    </div>
+    <p className={styles.time}>{convertDatetimeToTime(completeAt)}</p>
+  </li>
+);
+
+export default TaskLogItem;
