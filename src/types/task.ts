f4844617981c44a2dcb5ee8550765ca7820b8be3
minguu <minguu42@gmail.com>
2021-08-12 12:13:40 +0900

    不要なものを削除し, Task を再定義

---
+++ src/types/task.ts
@@ -0,0 +1,11 @@
+export type Task = {
+  id: number;
+  title: string;
+  expectedPomodoroNum: number;
+  actualPomodoroNum: number;
+  dueOn: string;
+  isCompleted: boolean;
+  completedAt: string;
+  createdAt: string;
+  updatedAt: string;
+};
