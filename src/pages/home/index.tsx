c5211683bb67139a9b3a4f9a8f09ed55ca1af6d4
minguu <minguu42@gmail.com>
2021-08-20 09:27:28 +0900

    タスクの完了を API と連携

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 
 import TopAppBar from "components/common/TopAppBar";
@@ -6,9 +7,8 @@ import AddTaskForm from "components/home/AddTaskForm";
 import TaskList from "components/home/TaskList";
 import PomodoroPlayer from "components/home/PomodoroPlayer";
 import styles from "pages/home/Home.module.scss";
-import { isJsonTask, isJsonTasks, Task, newTask } from "types/task";
-import { useEffect, useState } from "react";
-import { getData, postData } from "lib/fetch";
+import { Task, isJsonTask, isJsonTasks, newTask } from "types/task";
+import { getData, postData, patchData } from "lib/fetch";
 import { useAuth } from "contexts/AuthContext";
 
 type Props = {
@@ -65,7 +65,7 @@ const HomeContainer = (): JSX.Element => {
   const { currentUser } = useAuth();
 
   useEffect(() => {
-    getData("/tasks", currentUser)
+    getData("/tasks?is-completed=false", currentUser)
       .then((data) => {
         if (isJsonTasks(data)) {
           const tasks = data.tasks.map((task) => newTask(task));
@@ -83,7 +83,7 @@ const HomeContainer = (): JSX.Element => {
   const addTask = (task: Task): void => {
     const reqBody = {
       title: task.title,
-      expectedPomodoroNum: task.expectedPomodoroNumber ?? 0,
+      expectedPomodoroNumber: task.expectedPomodoroNumber ?? 0,
       dueOn: task.dueOn ?? "0001-01-01T00:00:00Z",
     };
     postData("/tasks", reqBody, currentUser)
@@ -102,8 +102,18 @@ const HomeContainer = (): JSX.Element => {
   };
 
   const completeTask = (task: Task): void => {
-    const tmp = tasks.filter((t) => t.id !== task.id);
-    setTasks(tmp);
+    const reqBody = {
+      isCompleted: true,
+    };
+    patchData("/tasks/" + String(task.id), reqBody, currentUser)
+      .then(() => {
+        const tmp = tasks.filter((t) => t.id !== task.id);
+        setTasks(tmp);
+      })
+      .catch((error) => {
+        window.alert("タスクの完了に失敗しました。もう一度お試しください");
+        console.log("patchTask failed:", error);
+      });
   };
 
   const setTask = (task: Task): void => {
