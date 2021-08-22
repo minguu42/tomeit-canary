cb9cdba0b4ccd2e9c044ad7d77f83a3c153d9693
minguu <minguu42@gmail.com>
2021-08-22 17:14:52 +0900

    微小な修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    reactStrictMode: true,
-    eslint: {
-        dirs: ["src/components", "src/contexts", "src/lib", "src/pages", "src/types"],
-    },
+  reactStrictMode: true,
+  eslint: {
+    dirs: [
+      "src/components",
+      "src/contexts",
+      "src/lib",
+      "src/pages",
+      "src/types",
+    ],
+  },
 };
