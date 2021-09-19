3577d433ebf965db806be63ce2d167db88440225
minguu <minguu42@gmail.com>
2021-09-19 20:00:47 +0900

    テスト環境の変更
    Next.js の公式ドキュメントに合わせたテスト環境に変更した.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,27 @@
 module.exports = {
-  preset: "ts-jest",
-  roots: ["<rootDir>/src"],
   testEnvironment: "jsdom",
-  setupFiles: ["<rootDir>/setEnvVars.js"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  collectCoverageFrom: [
+    "**/*.{jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/.next/**",
+  ],
   moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-    "^(pages|components|lib|contexts)/(.+)": "<rootDir>/src/$1/$2",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
+    "^contexts/(.*)$": "<rootDir>/src/contexts/$1",
+    "^lib/(.*)$": "<rootDir>/src/lib/$1",
+    "^models/(.*)$": "<rootDir>/src/models/$1",
+    "^pages/(.*)$": "<rootDir>/src/pages/$1",
   },
-  globals: {
-    "ts-jest": {
-      tsconfig: {
-        jsx: "react-jsx",
-      },
-    },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
