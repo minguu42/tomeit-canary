fb34df275129c55962c149723cbd7ed30634ecb1
minguu <minguu42@gmail.com>
2021-09-20 20:04:04 +0900

    TaskListHeader の追加

---
+++ src/components/common/TaskListHeader/index.tsx
@@ -0,0 +1,29 @@
+import s from "./styles.module.scss";
+import { Task, tasksState } from "models/task";
+import { formatToLocalDate } from "lib/format";
+import { useRecoilValue } from "recoil";
+
+type Props = {
+  title: string;
+  tasks: Task[];
+  date?: Date;
+};
+
+export const TaskListHeader = ({ title, tasks, date }: Props): JSX.Element => (
+  <div className={s.container}>
+    <div className={s.top}>
+      <h1 className={s.title}>{title}</h1>
+      <p className={s.date}>{date && formatToLocalDate(date)}</p>
+    </div>
+    <p className={s.taskNum}>タスク数：{tasks.length}</p>
+  </div>
+);
+
+const TaskListHeaderContainer = (): JSX.Element => {
+  const tasks = useRecoilValue(tasksState);
+  const today = new Date();
+
+  return <TaskListHeader title="今日" tasks={tasks} date={today} />;
+};
+
+export default TaskListHeaderContainer;
