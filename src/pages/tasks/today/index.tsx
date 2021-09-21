7a4f9818dd233c2b098f17d635f3fbbc1f9208b4
minguu <minguu42@gmail.com>
2021-09-21 21:02:53 +0900

    Today ページの修正

--- src/pages/tasks/today/index.tsx
+++ src/pages/tasks/today/index.tsx
@@ -1,27 +1,16 @@
-import { useEffect } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useSetRecoilState } from "recoil";
 
 import TasksPageLayout from "components/common/TasksPageLayout";
-import { tasksFilterState } from "models/task";
 
-const Today: NextPage = () => {
-  const setTasksFilter = useSetRecoilState(tasksFilterState);
+const Today: NextPage = () => (
+  <>
+    <Head>
+      <title>今日 - tomeit</title>
+    </Head>
 
-  useEffect(() => {
-    setTasksFilter("Today");
-  }, [setTasksFilter]);
-
-  return (
-    <>
-      <Head>
-        <title>今日 - tomeit</title>
-      </Head>
-
-      <TasksPageLayout />
-    </>
-  );
-};
+    <TasksPageLayout tasksFilter="Today" />
+  </>
+);
 
 export default Today;
