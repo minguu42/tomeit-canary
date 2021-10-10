d18d0a3dc7f2342a47857530fe8b202f0938a23c
minguu <minguu42@gmail.com>
2021-10-10 13:50:01 +0900

    DrawerLayout は main の中を children として受け取るように変更

--- src/components/common/DrawerLayout/index.tsx
+++ src/components/common/DrawerLayout/index.tsx
@@ -2,15 +2,16 @@ import TopAppBar from "components/common/DrawerLayout/TopAppBar";
 import Drawer from "components/common/DrawerLayout/Drawer";
 import s from "./styles.module.scss";
 
-const DrawerLayout = (): JSX.Element => (
+type Props = {
+  children: JSX.Element;
+};
+
+const DrawerLayout = ({ children }: Props): JSX.Element => (
   <>
     <TopAppBar />
     <div className={s.body}>
       <Drawer />
-      <main className={s.main}>
-        Hello, 世界!
-        absoluteだと親要素を基準に、絶対的な位置を指定することになります。たとえば「親要素の左上から下に10px、右に10pxの位置に配置する」というようなことができます。
-      </main>
+      <main className={s.main}>{children}</main>
     </div>
   </>
 );
