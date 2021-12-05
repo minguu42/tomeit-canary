28fd393621786b30da3134cb2c5fcbc5371f1262
minguu <minguu42@gmail.com>
2021-12-06 06:56:18 +0900

    テキストを追加

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { Container, Flex, VStack, Text, Heading } from "@chakra-ui/react";
 
 const Landing = (): JSX.Element => (
   <>
@@ -10,7 +11,20 @@ const Landing = (): JSX.Element => (
         content="tomeit は必要なことだけに集中するためのタスク管理アプリです。ポモドーロテクニックを使って、今やるべきことのみに集中し, 淡々とタスクをこなしましょう！"
       />
     </Head>
-    <h1>Hello, New Directory structure!</h1>
+
+    <Container maxW="container.xl">
+      <Flex>
+        <VStack alignItems="flex-start">
+          <Heading>やるべきことのみをやる</Heading>
+          <Text>
+            tomeit は必要なことだけに集中するためのタスク管理アプリです。
+            <br />
+            今やるべきことのみに集中し、淡々とタスクをこなしましょう！
+          </Text>
+        </VStack>
+        <h2>Be fun!</h2>
+      </Flex>
+    </Container>
   </>
 );
 
