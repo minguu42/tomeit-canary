a1930f7365072217c5d4abdf0d82145867deeee1
minguu <minguu42@gmail.com>
2021-12-06 21:54:04 +0900

    Drawer をレスポンシブ対応

--- src/components/common/LayoutWithDrawer.tsx
+++ src/components/common/LayoutWithDrawer.tsx
@@ -5,21 +5,40 @@ import {
   Spacer,
   Heading,
   IconButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 
-import { AccountCircleIcon, HistoryIcon } from "components/icons";
+import { AccountCircleIcon, HistoryIcon, MenuIcon } from "components/icons";
 import DrawerMenu from "components/common/DrawerMenu";
 import Navigation from "components/common/Navigation";
 
 type Props = {
   children: JSX.Element;
+  isOpen: boolean;
+  onToggle: () => void;
 };
 
-const LayoutWithDrawer = ({ children }: Props): JSX.Element => (
+type ContainerProps = {
+  children: JSX.Element;
+};
+
+const LayoutWithDrawer = ({
+  children,
+  isOpen,
+  onToggle,
+}: Props): JSX.Element => (
   <>
     <HStack px={4} bg="purple.500" h={14} spacing={4}>
       <Box d={{ base: "block", md: "none" }}>
         <DrawerMenu />
+      </Box>
+      <Box d={{ base: "none", md: "block" }}>
+        <IconButton
+          aria-label="ドロワーを切り替える"
+          colorScheme="purple"
+          icon={<MenuIcon />}
+          onClick={onToggle}
+        />
       </Box>
       <Heading as="h2" fontSize="lg">
         tomeit
@@ -38,11 +57,23 @@ const LayoutWithDrawer = ({ children }: Props): JSX.Element => (
     </HStack>
     <Flex>
       <Box d={{ base: "none", md: "block" }}>
-        <Navigation />
+        {isOpen ? <Navigation /> : <></>}
       </Box>
       {children}
     </Flex>
   </>
 );
 
-export default LayoutWithDrawer;
+const LayoutWithDrawerContainer = ({
+  children,
+}: ContainerProps): JSX.Element => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <LayoutWithDrawer isOpen={isOpen} onToggle={onToggle}>
+      {children}
+    </LayoutWithDrawer>
+  );
+};
+
+export default LayoutWithDrawerContainer;
