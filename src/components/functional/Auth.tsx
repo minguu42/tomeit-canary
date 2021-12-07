0c0075e9c738498fa5854b743ac98f2032622e8a
minguu <minguu42@gmail.com>
2021-12-07 19:52:35 +0900

    format

--- src/components/functional/Auth.tsx
+++ src/components/functional/Auth.tsx
@@ -1,22 +1,22 @@
-import {Center, Spinner, Text} from "@chakra-ui/react";
+import { Center, Spinner, Text } from "@chakra-ui/react";
 
 import { useAuth } from "lib/auth";
 
 type Props = {
-    children: JSX.Element;
+  children: JSX.Element;
 };
 
 const Auth = ({ children }: Props): JSX.Element => {
-    const isLoading = useAuth();
+  const isLoading = useAuth();
 
-    return isLoading ? (
-        <Center h="100vh">
-            <Spinner />
-            <Text ml="8px">Loading...</Text>
-        </Center>
-    ) : (
-        children
-    );
+  return isLoading ? (
+    <Center h="100vh">
+      <Spinner />
+      <Text ml="8px">Loading...</Text>
+    </Center>
+  ) : (
+    children
+  );
 };
 
 export default Auth;
