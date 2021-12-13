ec5cfd4e4d50d110daabc4b8b5793cfa85c27eef
minguu <minguu42@gmail.com>
2021-12-13 21:36:52 +0900

    feat: HistoryIcon, DarkModeIcon, LightModeIcon の追加

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,5 +1,8 @@
 export { default as CircleIcon } from "./CircleIcon";
+export { default as DarkModeIcon } from "./DarkModeIcon";
 export { default as GoogleIcon } from "./GoogleIcon";
+export { default as HistoryIcon } from "./HistoryIcon";
+export { default as LightModeIcon } from "./LightModeIcon";
 export { default as LogoutIcon } from "./LogoutIcon";
 export { default as MenuIcon } from "./MenuIcon";
 export { default as PlayCircleIcon } from "./PlayCircleIcon";
