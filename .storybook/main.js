2b2ec3288d5b2ae030b775c90153cfbf7beb669b
minguu <minguu42@gmail.com>
2021-12-19 09:57:03 +0900

    feat: Stobybook のセットアップ

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  typescript: { reactDocgen: false },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
