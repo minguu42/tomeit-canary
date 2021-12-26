50f5ccad1200b49847d776ed86b91a32a4cb564c
minguu <minguu42@gmail.com>
2021-12-27 08:14:57 +0900

    feat!: グローバル CSS の追加と適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
+import "@/styles/globals.css";
 import Auth from "components/functional/Auth";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
