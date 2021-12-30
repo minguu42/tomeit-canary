49fe6288eca6220f01f04ba9cbdd65e820651af2
minguu <minguu42@gmail.com>
2021-12-30 10:19:44 +0900

    fix!: アイコンコンポーネントの Props を変更
    size は Props で指定し、fill(color) は親要素で指定する。

--- src/components/common/icons/MenuIcon.tsx
+++ src/components/common/icons/MenuIcon.tsx
@@ -1,15 +1,14 @@
 type Props = {
-  className?: string;
+  size?: number;
 };
 
-const MenuIcon = ({ className }: Props): JSX.Element => (
+const MenuIcon = ({ size }: Props): JSX.Element => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24px"
-    height="24px"
+    width={size || 24}
+    height={size || 24}
     viewBox="0 0 24 24"
     fill="currentColor"
-    className={className}
   >
     <path d="M0 0h24v24H0V0z" fill="none" />
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
