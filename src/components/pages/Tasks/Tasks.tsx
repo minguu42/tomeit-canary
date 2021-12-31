16a52976715450b67fc95dcdc006554a525d1524
minguu <minguu42@gmail.com>
2021-12-31 16:45:29 +0900

    feat: Tasks ページの雛形の作成

---
+++ src/components/pages/Tasks/Tasks.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import Layout from "@/components/common/Layout";
+
+export const Tasks: NextPage = () => (
+  <Layout>
+    <Head>
+      <title>Tasks - tomeit</title>
+    </Head>
+    <p>Hello</p>
+    <p>Hello2</p>
+  </Layout>
+);
+
+export default Tasks;
