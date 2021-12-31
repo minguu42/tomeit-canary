209c6bba51e623e6a0c3898717b614155c685486
minguu <minguu42@gmail.com>
2021-12-31 16:43:55 +0900

    feat: <Layout> の雛形の作成

---
+++ src/components/common/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import TopAppBar from "@/components/common/TopAppBar";
+import Drawer from "@/components/common/Drawer/Drawer";
+
+type Props = {
+    children: JSX.Element[]
+}
+
+export const Layout = ({ children }: Props): JSX.Element => (
+    <div>
+        <TopAppBar />
+        <Drawer />
+        {children.map((child) => child)}
+    </div>
+)
+
+export default Layout;
