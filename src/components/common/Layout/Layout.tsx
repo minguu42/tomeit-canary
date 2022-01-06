18d35b1e5126381ccabb4da1479f73e2f5093a6f
minguu <minguu42@gmail.com>
2022-01-06 20:41:47 +0900

    fix: 一時的な変更

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -2,15 +2,15 @@ import TopAppBar from "@/components/common/TopAppBar";
 import Drawer from "@/components/common/Drawer/Drawer";
 
 type Props = {
-    children: JSX.Element[]
-}
+  children: JSX.Element[];
+};
 
 export const Layout = ({ children }: Props): JSX.Element => (
-    <div>
-        <TopAppBar />
-        <Drawer />
-        {children.map((child) => child)}
-    </div>
-)
+  <div>
+    <TopAppBar />
+    <Drawer />
+    {children.map((child) => child)}
+  </div>
+);
 
 export default Layout;
