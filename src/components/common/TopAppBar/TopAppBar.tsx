87d03782fec4d2cd0a7fad75cf8fd47abb86b9ca
minguu <minguu42@gmail.com>
2022-01-07 07:27:49 +0900

    feat: TopAppBar の追加

--- src/components/common/TopAppBar/TopAppBar.tsx
+++ src/components/common/TopAppBar/TopAppBar.tsx
@@ -1,59 +1,44 @@
+import type { VFC } from "react";
+
 import {
   MenuIcon,
   LightModeIcon,
   DarkModeIcon,
-  AccountCircleIcon,
+  LogoutIcon,
 } from "@/components/common/icons";
 import s from "./TopAppBar.module.css";
-import { Theme, useTheme } from "@/components/functional/Theme";
+import { useTheme } from "@/components/functional/Theme";
 
-type Props = {
-  theme: Theme;
-  toggleDrawer: () => void;
-  toggleTheme: () => void;
-  handleAccountClick: () => void;
-};
-
-export const TopAppBar = ({
-  theme,
-  toggleDrawer,
-  toggleTheme,
-  handleAccountClick,
-}: Props): JSX.Element => (
-  <header className={s.container}>
-    <button className={s.iconButton} onClick={toggleDrawer}>
-      <div className={s.iconButtonLayer}>
-        <MenuIcon />
-      </div>
-    </button>
-    <h2 className={s.headline}>tomeit</h2>
-    <button className={s.iconButton} onClick={toggleTheme}>
-      <div className={s.iconButtonLayer}>
-        {theme === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
-      </div>
-    </button>
-    <button className={s.iconButton} onClick={handleAccountClick}>
-      <div className={s.iconButtonLayer}>
-        <AccountCircleIcon size={32} />
-      </div>
-    </button>
-  </header>
-);
-
-const TopAppBarContainer = (): JSX.Element => {
+const TopAppBar: VFC = () => {
+  const headline = "tomeit";
   const { theme, toggleTheme } = useTheme();
+  const toggleNavigationDrawer = () => {
+    window.alert("toggleNavigationDrawer");
+  };
+  const logout = () => {
+    window.alert("ログアウト");
+  };
+
   return (
-    <TopAppBar
-      theme={theme}
-      toggleDrawer={() => {
-        window.alert("メニュークリック");
-      }}
-      toggleTheme={toggleTheme}
-      handleAccountClick={() => {
-        window.alert("アカウントボタンクリックテスト");
-      }}
-    />
+    <header className={s.container}>
+      <button onClick={toggleNavigationDrawer} className={s.navigationIcon}>
+        <div className={s.navigationIconLayer} />
+        <MenuIcon />
+      </button>
+      <h2 className={s.headline}>{headline}</h2>
+      <div className={s.spacer} />
+      <div className={s.trailingIcons}>
+        <button onClick={toggleTheme} className={s.interactiveIcon}>
+          <div className={s.interactiveIconLayer} />
+          {theme === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
+        </button>
+        <button onClick={logout} className={s.interactiveIcon}>
+          <div className={s.interactiveIconLayer} />
+          <LogoutIcon />
+        </button>
+      </div>
+    </header>
   );
 };
 
-export default TopAppBarContainer;
+export default TopAppBar;
