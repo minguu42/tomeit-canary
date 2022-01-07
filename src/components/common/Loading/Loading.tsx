e8dca73b55ee52ebb0d9f5a9f3cc7c10928ad840
minguu <minguu42@gmail.com>
2022-01-08 08:56:19 +0900

    feat: Loading の追加

---
+++ src/components/common/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import type { VFC } from "react";
+
+import s from "./Loading.module.css";
+
+const Loading: VFC = () => {
+  return (
+    <div className={s.background}>
+      <h1 className={s.message}>Loading ...</h1>
+    </div>
+  );
+};
+
+export default Loading;
