310feef96bf93bd3007fd9ed1529147d4ae751c4
minguu <minguu42@gmail.com>
2022-01-08 12:49:35 +0900

    feat: Noto Sans JP フォントの追加と適応

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
