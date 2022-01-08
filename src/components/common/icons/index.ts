5818dd62b87e8f53b091e643f41685f6ad5a63bd
minguu <minguu42@gmail.com>
2022-01-08 12:20:57 +0900

    feat: TaskListItem で必要なアイコンを追加

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,4 +1,7 @@
+export { default as CircleIcon } from "./CircleIcon";
 export { default as DarkModeIcon } from "./DarkModeIcon";
 export { default as LightModeIcon } from "./LightModeIcon";
 export { default as LogoutIcon } from "./LogoutIcon";
 export { default as MenuIcon } from "./MenuIcon";
+export { default as PlayCircleIcon } from "./PlayCircleIcon";
+export { default as TimerIcon } from "./TimerIcon";
