2d5168112017dac0c2b4aec4f44e7aecc381983b
minguu <minguu42@gmail.com>
2022-01-09 13:05:08 +0900

    feat: PomodoroTimer で使用するアイコンの追加

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,9 +1,12 @@
 export { default as AddTaskIcon } from "./AddTask";
 export { default as AlarmIcon } from "./Alarm";
 export { default as AlarmOnIcon } from "./AlarmOn";
+export { default as CheckCircleIcon } from "./CheckCircle";
 export { default as CircleIcon } from "./Circle";
 export { default as DarkModeIcon } from "./DarkMode";
 export { default as LightModeIcon } from "./LightMode";
 export { default as LogoutIcon } from "./Logout";
 export { default as MenuIcon } from "./Menu";
+export { default as PauseCircleIcon } from "./PauseCircle";
 export { default as PlayCircleIcon } from "./PlayCircle";
+export { default as StopCircleIcon } from "./StopCircle";
