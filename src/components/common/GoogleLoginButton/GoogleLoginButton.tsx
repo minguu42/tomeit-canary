641ff08f566c49edf8c54b1ff583d9e0577ab3ba
minguu <minguu42@gmail.com>
2022-01-11 10:58:56 +0900

    feat: login の適用

--- src/components/common/GoogleLoginButton/GoogleLoginButton.tsx
+++ src/components/common/GoogleLoginButton/GoogleLoginButton.tsx
@@ -1,33 +1,24 @@
 import Image from "next/image";
 
 import s from "./GoogleLoginButton.module.css";
+import { login } from "@/lib/auth";
 
-type Props = {
-  handleLogin: () => void;
+export const GoogleLoginButton = (): JSX.Element => {
+  return (
+    <button onClick={login} className={s.container}>
+      <div className={s.layer} />
+      <div className={s.logoBackground}>
+        <Image
+          src="/images/google.png"
+          width={18}
+          height={18}
+          layout="fixed"
+          alt="Google ロゴ"
+        />
+      </div>
+      Sign in with Google
+    </button>
+  );
 };
 
-export const GoogleLoginButton = ({ handleLogin }: Props): JSX.Element => (
-  <button className={s.container}>
-    <div className={s.layer} />
-    <div className={s.logoBackground}>
-      <Image
-        src="/images/google.png"
-        width={18}
-        height={18}
-        layout="fixed"
-        alt="Google ロゴ"
-        onClick={handleLogin}
-      />
-    </div>
-    Sign in with Google
-  </button>
-);
-
-const GoogleLoginButtonContainer = () => {
-  const handleLogin = () => {
-    console.log("test");
-  };
-  return <GoogleLoginButton handleLogin={handleLogin} />;
-};
-
-export default GoogleLoginButtonContainer;
+export default GoogleLoginButton;
