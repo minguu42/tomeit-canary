74c9b07bca674989e52abcbd8aa45a2ebb037d8e
minguu <minguu42@gmail.com>
2022-01-12 22:06:15 +0900

    feat: ユーザによるアクセス制御の追加

--- src/components/pages/Tasks/Tasks.tsx
+++ src/components/pages/Tasks/Tasks.tsx
@@ -5,21 +5,26 @@ import TaskAddForm from "@/components/models/task/TaskAddForm";
 import TaskList from "@/components/models/task/TaskList";
 import PomodoroTimer from "@/components/models/pomodoro/PomodoroTimer";
 import s from "./Tasks.module.css";
+import { useRequiredLogin } from "@/lib/auth";
 
-export const Tasks: NextPage = () => (
-  <>
-    <Head>
-      <title>Tasks - tomeit</title>
-    </Head>
+export const Tasks: NextPage = () => {
+  useRequiredLogin();
 
-    <main className={s.container}>
-      <PomodoroTimer />
-      <div className={s.taskField}>
-        <TaskAddForm />
-        <TaskList />
-      </div>
-    </main>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <title>Tasks - tomeit</title>
+      </Head>
+
+      <main className={s.container}>
+        <PomodoroTimer />
+        <div className={s.taskField}>
+          <TaskAddForm />
+          <TaskList />
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Tasks;
