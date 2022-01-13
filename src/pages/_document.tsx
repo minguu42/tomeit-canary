49d7046d5c3a635df67cb5420789acab29a06614
minguu <minguu42@gmail.com>
2022-01-13 10:13:09 +0900

    feat: ブラウザにテーマを伝えるために color-scheme の追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+          <meta name="color-scheme" content="light dark" />
         </Head>
         <body>
           <Main />
