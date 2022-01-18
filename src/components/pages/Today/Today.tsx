195b0d5edd454a5154b8cf69a6dc81b07be1d311
minguu <minguu42@gmail.com>
2022-01-18 20:56:12 +0900

    fix!: TasksPage コンポーネントを使用し、再実装

--- src/components/pages/Today/Today.tsx
+++ src/components/pages/Today/Today.tsx
@@ -1,28 +1,9 @@
 import { NextPage } from "next";
-import Head from "next/head";
 
-import PomodoroTimer from "@/components/models/pomodoro/PomodoroTimer";
-import TaskAddForm from "@/components/models/task/TaskAddForm";
-import s from "./Today.module.css";
-import { useRequiredLogin } from "@/lib/auth";
+import TasksPage from "@/components/models/task/TasksPage";
 
 export const Today: NextPage = () => {
-  useRequiredLogin();
-
-  return (
-    <>
-      <Head>
-        <title>今日 - tomeit</title>
-      </Head>
-
-      <main className={s.container}>
-        <PomodoroTimer />
-        <div className={s.taskField}>
-          <TaskAddForm />
-        </div>
-      </main>
-    </>
-  );
+  return <TasksPage title="今日 - tomeit" filter="today" />;
 };
 
 export default Today;
--- src/components/pages/Tomorrow/Tomorrow.tsx
+++ src/components/pages/Tomorrow/Tomorrow.tsx
@@ -1,49 +1,9 @@
 import { NextPage } from "next";
-import Head from "next/head";
 
-import PomodoroTimer from "@/components/models/pomodoro/PomodoroTimer";
-import TaskAddForm from "@/components/models/task/TaskAddForm";
-import TaskList from "@/components/models/task/TaskList";
-import s from "./Tomorrow.module.css";
-import { useRequiredLogin } from "@/lib/auth";
-import { useTasksActions } from "@/globalStates/tasksAtom";
-import { useState } from "react";
-import { Task } from "@/models/task";
+import TasksPage from "@/components/models/task/TasksPage";
 
 export const Tomorrow: NextPage = () => {
-  useRequiredLogin();
-  const { replaceTask } = useTasksActions();
-  const [featuredTask, setFeaturedTask] = useState<Task | null>(null);
-
-  const onCompleteTaskButtonClick = (task: Task): void => {
-    if (task === null) return;
-    const newTask: Task = { ...task, isCompleted: true };
-    replaceTask(task, newTask);
-    if (featuredTask !== null && task.id === featuredTask.id) {
-      setFeaturedTask(null);
-    }
-  };
-
-  return (
-    <>
-      <Head>
-        <title>明日 - tomeit</title>
-      </Head>
-
-      <main className={s.container}>
-        <PomodoroTimer />
-        <div className={s.taskField}>
-          <TaskAddForm />
-          <TaskList
-            filter="tomorrow"
-            featuredTask={featuredTask}
-            setFeaturedTask={setFeaturedTask}
-            onCompleteTaskButtonClick={onCompleteTaskButtonClick}
-          />
-        </div>
-      </main>
-    </>
-  );
+  return <TasksPage title="明日 - tomeit" filter="tomorrow" />;
 };
 
 export default Tomorrow;
