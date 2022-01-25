9e2ee8fe25e235c4d5fe406cd76ac5cb9330195a
minguu <minguu42@gmail.com>
2022-01-25 23:33:28 +0900

    fix!: userAtom.ts は read hook と write hook のみを返すように変更

--- src/globalStates/userAtom.ts
+++ src/globalStates/userAtom.ts
@@ -1,7 +1,12 @@
+import { useCallback } from "react";
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
 import { User } from "firebase/auth";
 
 type UserAtom = User | null;
+
+type UserMutators = {
+  setUser: (user: UserAtom) => void;
+};
 
 const userAtom = atom<UserAtom>({
   key: "userAtom",
@@ -9,10 +14,17 @@ const userAtom = atom<UserAtom>({
   dangerouslyAllowMutability: true,
 });
 
-export const useUserAtom = (): UserAtom => {
-  return useRecoilValue(userAtom);
-};
+export const useUserAtom = (): UserAtom => useRecoilValue(userAtom);
 
-export const useSetUserAtom = () => {
-  return useSetRecoilState(userAtom);
+export const useUserMutators = (): UserMutators => {
+  const setAtom = useSetRecoilState(userAtom);
+
+  const setUser = useCallback(
+    (user: UserAtom) => {
+      setAtom(user);
+    },
+    [setAtom]
+  );
+
+  return { setUser };
 };
