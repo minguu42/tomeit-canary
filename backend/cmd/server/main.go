6a45a7c113ad8c42718963d17ad4240a001e54da
minguu <minguu42@gmail.com>
2022-02-12 12:45:05 +0900

    fix!: 実行を確認するために一部のコードをコメントアウト

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,39 +4,33 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/minguu42/tomeit-api"
-
-	"github.com/go-chi/cors"
-
-	"github.com/go-chi/render"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/minguu42/tomeit"
 )
 
 func main() {
-	firebaseApp := tomeit.InitFirebaseApp()
+	//firebaseApp := tomeit.InitFirebaseApp()
 
-	db := tomeit.OpenDB(os.Getenv("DSN"))
-	defer tomeit.CloseDB(db)
+	//db := tomeit.OpenDB(os.Getenv("DSN"))
+	//defer tomeit.CloseDB(db)
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   strings.Split(os.Getenv("ALLOW_ORIGINS"), ","),
-		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-	}))
-	r.Use(tomeit.UserCtx(db, firebaseApp))
+	//r.Use(render.SetContentType(render.ContentTypeJSON))
+	//r.Use(cors.Handler(cors.Options{
+	//	AllowedOrigins:   strings.Split(os.Getenv("ALLOW_ORIGINS"), ","),
+	//	AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "OPTIONS"},
+	//	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	//	ExposedHeaders:   []string{"Link"},
+	//	AllowCredentials: true,
+	//}))
+	//r.Use(tomeit.UserCtx(db, firebaseApp))
 
-	tomeit.Route(r, db)
+	tomeit.Route(r) // tomeit.Route(r, db)
 
 	port := os.Getenv("PORT")
 	if port == "" {
--- backend/routes.go
+++ backend/routes.go
@@ -2,20 +2,23 @@ package tomeit
 
 import "github.com/go-chi/chi/v5"
 
-func Route(r chi.Router, db dbInterface) {
-	r.Route("/tasks", func(r chi.Router) {
-		r.Post("/", postTasks(db))
-		r.Get("/", getTasks(db))
-		r.Patch("/{taskID}", patchTask(db))
-		r.Put("/{taskID}", putTask(db))
-		r.Delete("/{taskID}", deleteTask(db))
+func Route(r chi.Router) { // func Route(r chi.Router, db dbInterface) {
+	r.Route("/v0", func(r chi.Router) {
+		r.Get("/health", getHealth)
 	})
+	//r.Route("/tasks", func(r chi.Router) {
+	//	r.Post("/", postTasks(db))
+	//	r.Get("/", getTasks(db))
+	//	r.Patch("/{taskID}", patchTask(db))
+	//	r.Put("/{taskID}", putTask(db))
+	//	r.Delete("/{taskID}", deleteTask(db))
+	//})
 
-	r.Route("/pomodoros", func(r chi.Router) {
-		r.Post("/", postPomodoros(db))
-		r.Get("/", getPomodoros(db))
-		r.Delete("/{pomodoroID}", deletePomodoro(db))
-
-		r.Get("/rest-count", getRestCount)
-	})
+	//r.Route("/pomodoros", func(r chi.Router) {
+	//	r.Post("/", postPomodoros(db))
+	//	r.Get("/", getPomodoros(db))
+	//	r.Delete("/{pomodoroID}", deletePomodoro(db))
+	//
+	//	r.Get("/rest-count", getRestCount)
+	//})
 }
