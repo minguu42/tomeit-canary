044ce07ba48a51e45d880c08894b84fc14b85d55
minguu <minguu42@gmail.com>
2022-02-17 21:59:53 +0900

    fix!: ロガーの初期化処理の追加とロギングの変更

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/minguu42/tomeit"
+	"github.com/minguu42/tomeit/logging"
 )
 
 func main() {
-	//firebaseApp := tomeit.InitFirebaseApp()
+	logging.InitLogger()
 
+	//firebaseApp, err := tomeit.InitFirebaseApp()
+	//if err != nil {
+	//	logging.Error.Fatalln("tomeit.InitFirebaseApp failed:", err)
+	//}
+	//
 	//db := tomeit.OpenDB(os.Getenv("DSN"))
 	//defer tomeit.CloseDB(db)
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
+	//r.Use(middlewares.Auth(db, firebaseApp))
 	r.Use(middleware.Recoverer)
 	//r.Use(render.SetContentType(render.ContentTypeJSON))
 	//r.Use(cors.Handler(cors.Options{
@@ -28,16 +34,15 @@ func main() {
 	//	ExposedHeaders:   []string{"Link"},
 	//	AllowCredentials: true,
 	//}))
-	//r.Use(tomeit.UserCtx(db, firebaseApp))
 
 	tomeit.Route(r) // tomeit.Route(r, db)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		logging.Error.Fatalln("$PORT must be set")
 	}
 
 	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatal("ListenAndServe failed:", err)
+		logging.Error.Fatalln("http.ListenAndServe failed:", err)
 	}
 }
