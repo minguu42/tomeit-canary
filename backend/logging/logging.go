90458cfabc9f3a47d473db645b202296600bdef8
minguu <minguu42@gmail.com>
2022-02-17 21:45:35 +0900

    feat: ロガーを定義する logging.go の追加

---
+++ backend/logging/logging.go
@@ -0,0 +1,18 @@
+package logging
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	Error *log.Logger
+	Info  *log.Logger
+	DEBUG *log.Logger
+)
+
+func InitLogger() {
+	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "INFO : ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
