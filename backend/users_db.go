bb3bd97699a337ea28fb4b9fdc4bb63d59e9661b
minguu <minguu42@gmail.com>
2022-02-21 07:55:21 +0900

    fix: CreateUser、GetUserByDigestUID を goqu で再実装

--- backend/users_db.go
+++ backend/users_db.go
@@ -3,6 +3,9 @@ package tomeit
 import (
 	"fmt"
 	"time"
+
+	"github.com/doug-martin/goqu/v9"
+	"github.com/minguu42/tomeit/logger"
 )
 
 type UserDBInterface interface {
@@ -14,25 +17,42 @@ type UserDBInterface interface {
 func (db *DB) CreateUser(digestUID string) (*User, error) {
 	createdAt := time.Now()
 
+	sql, _, err := db.dialect.Insert("users").Cols("digest_uid", "created_at", "updated_at").Vals(goqu.Vals{digestUID, createdAt, createdAt}).ToSQL()
+	if err != nil {
+		return nil, fmt.Errorf("ds.ToSQL failed: %w", err)
+	}
+	result, err := db.db.Exec(sql)
+	if err != nil {
+		return nil, fmt.Errorf("db.Exec failed: %w", err)
+	}
+	logger.Debug.Println(sql)
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, fmt.Errorf("result.LastInsertId failed: %w", err)
+	}
+
 	user := User{
+		ID:        int(id),
 		DigestUID: digestUID,
 		RestCount: 4,
 		CreatedAt: createdAt,
 		UpdatedAt: createdAt,
 	}
-	if err := db.Create(&user).Error; err != nil {
-		return nil, fmt.Errorf("db.Create failed: %w", err)
-	}
-
 	return &user, nil
 }
 
 func (db *DB) GetUserByDigestUID(digestUID string) (*User, error) {
 	var user User
 
-	if err := db.Where("digest_uid", digestUID).First(&user).Error; err != nil {
-		return nil, fmt.Errorf("db.First failed: %w", err)
+	sql, _, err := db.dialect.From("user").Select(&User{}).Where(goqu.Ex{"digest_uid": digestUID}).ToSQL()
+	if err != nil {
+		return nil, fmt.Errorf("ds.ToSQL failed: %w", err)
 	}
+	if err := db.db.QueryRow(sql).Scan(&user.ID, &user.DigestUID, &user.RestCount, &user.CreatedAt, &user.UpdatedAt); err != nil {
+		return nil, fmt.Errorf("row.Scan failed: %w", err)
+	}
+	logger.Debug.Println(sql)
 
 	return &user, nil
 }
