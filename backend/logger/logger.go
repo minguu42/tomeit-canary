43d386c4a89cf69fb1dff3bf04b64a241d6f12be
minguu <minguu42@gmail.com>
2022-02-21 07:37:01 +0900

    fix: タイポの修正

--- backend/logger/logger.go
+++ backend/logger/logger.go
@@ -8,11 +8,11 @@ import (
 var (
 	Error *log.Logger
 	Info  *log.Logger
-	DEBUG *log.Logger
+	Debug *log.Logger
 )
 
 func InitLogger() {
 	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Info = log.New(os.Stdout, "INFO : ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Debug = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
