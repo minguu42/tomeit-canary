4f18267b5b06eaa76c36aea6f9357ed290d6c66d
minguu <minguu42@gmail.com>
2022-02-21 17:18:17 +0900

    feat: /v0/health はユーザ認証を必要としないように変更

--- backend/auth.go
+++ backend/auth.go
@@ -17,6 +17,11 @@ var userKey key
 func Auth(db dbInterface, firebaseApp firebaseAppInterface) func(handler http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/v0/health" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
 			authorization := strings.Split(r.Header.Get("Authorization"), " ")
 			if authorization[0] != "Bearer" {
 				logger.Error.Println("JWT is required.")
