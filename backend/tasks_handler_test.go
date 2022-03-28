46dbed30d41f8d6b24fe33c1188a66e04a88ee63
minguu42 <minguu42@gmail.com>
2022-03-29 06:44:20 +0900

    feat: postTasks の異常系テストを追加

--- backend/tasks_handler_test.go
+++ backend/tasks_handler_test.go
@@ -15,7 +15,7 @@ var opt = cmpopts.IgnoreTypes(time.Time{})
 func TestPostTasks(t *testing.T) {
 	setupTestDB(t)
 	t.Cleanup(teardownTestDB)
-	t.Run("タスクを作成する", func(t *testing.T) {
+	t.Run("タスクを作成する（title）", func(t *testing.T) {
 		reqBody := strings.NewReader(`{"title": "タスク1"}`)
 		resp, body := doTestRequest(t, "POST", "/v0/tasks", nil, reqBody, "taskResponse")
 
@@ -37,6 +37,46 @@ func TestPostTasks(t *testing.T) {
 		}
 		if diff := cmp.Diff(got, want, opt); diff != "" {
 			t.Errorf("postTasks response mismatch (-got +want):\n%s", diff)
+		}
+	})
+	t.Run("タスクを作成する（title, estimatedPomoNum, dueOn）", func(t *testing.T) {
+		reqBody := strings.NewReader(`{"title": "タスク2", "estimatedPomoNum": 4, "dueOn": "2022-01-02T21:22:23Z"}`)
+		resp, body := doTestRequest(t, "POST", "/v0/tasks", nil, reqBody, "taskResponse")
+
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Status code should be %v, but %v", http.StatusCreated, resp.StatusCode)
+		}
+
+		got, ok := body.(taskResponse)
+		if !ok {
+			t.Fatal("Type Assertion failed.")
+		}
+		want := taskResponse{
+			ID:               2,
+			Title:            "タスク2",
+			EstimatedPomoNum: 4,
+			CompletedPomoNum: 0,
+			DueOn:            "2022-01-02T21:22:23Z",
+			CompletedOn:      "",
+		}
+		if diff := cmp.Diff(got, want, opt); diff != "" {
+			t.Errorf("postTasks response mismatch (-got +want):\n%s", diff)
+		}
+	})
+	t.Run("タスクの作成に失敗する（title がない）", func(t *testing.T) {
+		reqBody := strings.NewReader(`{"estimatedPomoNum": 4, "dueOn": "2022-01-02T21:22:23Z"}`)
+		resp, _ := doTestRequest(t, "POST", "/v0/tasks", nil, reqBody, "taskResponse")
+
+		if resp.StatusCode != http.StatusBadRequest {
+			t.Errorf("Status code should be %v, but %v", http.StatusBadRequest, resp.StatusCode)
+		}
+	})
+	t.Run("タスクの作成に失敗する（dueOn の形式が RFC3339 でない）", func(t *testing.T) {
+		reqBody := strings.NewReader(`{"title": "タスク3", "dueOn": "2022-01-02"}`)
+		resp, _ := doTestRequest(t, "POST", "/v0/tasks", nil, reqBody, "taskResponse")
+
+		if resp.StatusCode != http.StatusBadRequest {
+			t.Errorf("Status code should be %v, but %v", http.StatusBadRequest, resp.StatusCode)
 		}
 	})
 }
