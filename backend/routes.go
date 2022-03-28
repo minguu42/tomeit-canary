b79448514d2eae62144e333446187917ddb1d4a0
minguu42 <minguu42@gmail.com>
2022-03-28 09:08:22 +0900

    feat: postTasks 関数をルーティングに追加

--- backend/routes.go
+++ backend/routes.go
@@ -2,8 +2,12 @@ package tomeit
 
 import "github.com/go-chi/chi/v5"
 
-func Route(r chi.Router, _ *db) {
+func Route(r chi.Router, db *db) {
 	r.Route("/v0", func(r chi.Router) {
+		r.Route("/tasks", func(r chi.Router) {
+			r.Post("/", postTasks(db))
+		})
+
 		r.Get("/healthz", getHealthz)
 	})
 }
