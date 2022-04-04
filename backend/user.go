8d641e03c88073d3f365f9689c7a8e28216f7e51
minguu42 <minguu42@gmail.com>
2022-04-04 20:40:33 +0900

    fix!: User をアンエクスポート

--- backend/user.go
+++ backend/user.go
@@ -2,7 +2,7 @@ package tomeit
 
 import "time"
 
-type User struct {
+type user struct {
 	ID        int       `db:"id"`
 	DigestUID string    `db:"digest_uid"`
 	RestCount int       `db:"rest_count"`
