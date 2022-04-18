405aac2c102f8bb9f43dbf8d66122cca919f3baa
minguu42 <minguu42@gmail.com>
2022-04-19 07:23:52 +0900

    feat: fetchTasks の実装

--- src/models/task/fetch.ts
+++ src/models/task/fetch.ts
@@ -1,6 +1,11 @@
 import { User } from "firebase/auth";
 
-import { isTaskResponse, TaskResponse } from "@/models/task";
+import {
+  isTaskResponse,
+  isTasksResponse,
+  TaskResponse,
+  TasksResponse,
+} from "@/models/task";
 import { formatDateStringToRFC3339 } from "@/lib/format";
 
 const TOMEIT_API_URL = "http://localhost:8080/v0";
@@ -43,6 +48,29 @@ export const postTasks = async (
   const data: unknown = await response.json();
 
   if (isTaskResponse(data)) {
+    return data;
+  }
+  return errorResponse;
+};
+
+export const fetchTasks = async (user: User | null): Promise<TasksResponse> => {
+  const errorResponse: TasksResponse = { tasks: [] };
+  if (!user) {
+    return errorResponse;
+  }
+
+  const idToken = await user.getIdToken(true);
+  const response = await fetch(TOMEIT_API_URL + "/tasks", {
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      Authorization: "Bearer " + idToken,
+    },
+  });
+  const data: unknown = await response.json();
+
+  if (isTasksResponse(data)) {
     return data;
   }
   return errorResponse;
