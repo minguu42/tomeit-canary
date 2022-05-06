de0a2095851426d3e969dfa8a349323a3f7c7a58
minguu42 <minguu42@gmail.com>
2022-05-06 22:21:08 +0900

    feat: postTasks の追加

--- src/hooks/fetch.ts
+++ src/hooks/fetch.ts
@@ -1,9 +1,12 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 
 import { getIDToken } from "@/lib/auth";
-import { isTasksResponse, newTask, Task } from "@/types/task";
+import { formatDateStringToRFC3339 } from "@/lib/formatDate";
+import { isTaskResponse, isTasksResponse, newTask, Task } from "@/types/task";
 
-const TOMEIT_API_URL = process.env.NEXT_PUBLIC_TOMEIT_API_URL ?? "";
+const TOMEIT_API_URL =
+  process.env.NEXT_PUBLIC_TOMEIT_API_URL ?? "http://localhost:8080/v0";
+const TOMEIT_API_TASKS_URL = `${TOMEIT_API_URL}/tasks`;
 
 const fetcher = async (url: string) => {
   const idToken = await getIDToken(true);
@@ -23,19 +26,69 @@ const fetcher = async (url: string) => {
   if (isTasksResponse(data)) {
     return data.tasks.map((t) => newTask(t));
   } else {
-    throw new Error("Response is wrong.");
+    throw new Error("タスクの取得に失敗しました。");
   }
 };
 
 export const useTasks = () => {
-  const { data, error } = useSWR<Task[], Error>(
-    `${TOMEIT_API_URL}/tasks`,
-    fetcher
-  );
+  const { data, error } = useSWR<Task[], Error>(TOMEIT_API_TASKS_URL, fetcher);
 
   return {
     tasks: data ?? [],
     isLoading: error == undefined && data == undefined,
     isError: error != undefined,
+  };
+};
+
+type TaskActions = {
+  postTasks: (
+    title: string,
+    estimatedPomoNum: number,
+    dueOn: string
+  ) => Promise<void>;
+};
+
+export const useTaskActions = (): TaskActions => {
+  const { mutate } = useSWRConfig();
+
+  const postTasks = async (
+    title: string,
+    estimatedPomoNum: number,
+    dueOn: string
+  ): Promise<void> => {
+    const idToken = await getIDToken(true);
+    mutate<Task[]>(TOMEIT_API_TASKS_URL, async (tasks) => {
+      const res = await fetch(TOMEIT_API_TASKS_URL, {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          estimatedPomoNum: estimatedPomoNum,
+          dueOn: formatDateStringToRFC3339(dueOn),
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error("タスクの作成に失敗しました。");
+      }
+
+      const data: unknown = await res.json();
+      if (tasks && isTaskResponse(data)) {
+        return [...tasks, newTask(data)];
+      } else {
+        throw new Error("タスクの作成に失敗しました。");
+      }
+    }).catch((error) => {
+      throw error;
+    });
+  };
+
+  return {
+    postTasks,
   };
 };
