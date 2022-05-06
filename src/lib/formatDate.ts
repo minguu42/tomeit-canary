2692ea0abc4158d3f1dadc891b4ec35a9012bd3e
minguu42 <minguu42@gmail.com>
2022-05-06 22:16:03 +0900

    fix!: format.ts を formatDate.ts に変更し、より単純な関数に変更

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,16 +1,9 @@
-export const formatTimerTime = (seconds: number): string => {
+export const formatSecondsToMinutesSeconds = (seconds: number): string => {
   const mmNum = Math.floor(seconds / 60);
   const ssNum = seconds % 60;
   const mm = String(mmNum).padStart(2, "0");
   const ss = String(ssNum).padStart(2, "0");
   return `${mm}:${ss}`;
-};
-
-export const formatDate = (date: Date): string => {
-  const yyyy = String(date.getFullYear()).padStart(4, "0");
-  const mm = String(date.getMonth() + 1).padStart(2, "0");
-  const dd = String(date.getDate()).padStart(2, "0");
-  return `${yyyy}-${mm}-${dd}`;
 };
 
 export const formatDateToJP = (date: Date): string => {
