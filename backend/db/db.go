a9fa563222d3839d8e1733b1206c70d60e9921f3
minguu42 <minguu42@gmail.com>
2022-06-08 12:11:29 +0900

    feat: db パッケージの追加

---
+++ backend/db/db.go
@@ -0,0 +1,29 @@
+// Package db は DB を扱うパッケージ
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// NewDB はデータベースに接続し、 *sql.DB を返す。
+func NewDB(driverName, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(driverName, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database. %w", err)
+	}
+
+	for count := 0; count < 6; count++ {
+		if err := db.Ping(); err == nil {
+			return db, nil
+		}
+		log.Println("connection to database does not exit. check again after 5 seconds.")
+		time.Sleep(time.Second * 5)
+	}
+
+	return nil, fmt.Errorf("failed to connect database. %w", err)
+}
