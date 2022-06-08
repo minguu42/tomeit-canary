d079e5bfb4cbd1ad72a20804d569d11a5a895781
minguu42 <minguu42@gmail.com>
2022-06-08 12:20:31 +0900

    feat!: handler パッケージの追加

---
+++ backend/handler/handler.go
@@ -0,0 +1,7 @@
+package handler
+
+type handler struct{}
+
+func New() *handler {
+	return &handler{}
+}
---
+++ backend/handler/utils.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// writeResponse はレスポンスのヘッダにステータスコード、ボディに JSON データを書き込む。
+func writeResponse(w http.ResponseWriter, statusCode int, body interface{}) error {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(body); err != nil {
+		return fmt.Errorf("encoder.Encode failed: %w", err)
+	}
+	return nil
+}
