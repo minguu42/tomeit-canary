0c62d67c33a2e346db96073504aa3cabf65a298f
minguu42 <minguu42@gmail.com>
2022-06-09 11:24:42 +0900

    feat!: NewDialect 関数の追加

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,23 +2,32 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// NewDialect は MySQL のクエリを生成する goqu.DialectWrapper を返す。
+func NewDialect() *goqu.DialectWrapper {
+	dialect := goqu.Dialect("mysql")
+	return &dialect
+}
+
 // NewDB は MySQL に接続し、sql.DB を返す。
-func NewDB(dsn string) (*sql.DB, error) {
+func NewDB(ctx context.Context, dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database. %w", err)
 	}
 
 	for count := 0; count < 6; count++ {
-		if err := db.Ping(); err == nil {
+		if err := db.PingContext(ctx); err == nil {
 			return db, nil
 		}
 		log.Println("connection to database does not exit. check again after 5 seconds.")
