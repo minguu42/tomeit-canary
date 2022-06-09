347c3608c6b31b07b52711086a4ff3ca58cca476
minguu42 <minguu42@gmail.com>
2022-06-09 11:25:56 +0900

    feat!: firebase パッケージの追加

--- backend/firebase/firebase.go
+++ backend/firebase/firebase.go
@@ -1,3 +1,4 @@
+// Package firebase は Firebase を扱うパッケージ
 package firebase
 
 import (
@@ -8,8 +9,8 @@ import (
 	"google.golang.org/api/option"
 )
 
-// NewFirebaseApp は Firebase の認証を行い、firebase.App を返す。
-func NewFirebaseApp(ctx context.Context, googleCredentialsJSON string) (*firebase.App, error) {
+// NewApp は Firebase の認証を行い、firebase.App を返す。
+func NewApp(ctx context.Context, googleCredentialsJSON string) (*firebase.App, error) {
 	app, err := firebase.NewApp(ctx, nil, option.WithCredentialsJSON([]byte(googleCredentialsJSON)))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create firebase app. %w", err)
