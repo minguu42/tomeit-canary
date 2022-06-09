afaf1ee40f3e7f1221ce913ccd6c8bfd7b76f159
minguu42 <minguu42@gmail.com>
2022-06-09 12:22:06 +0900

    feat!: Handler に Route メソッドの追加

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -1,7 +1,10 @@
 // Package handler は各エンドポイントに対応するハンドラを定義するパッケージ
 package handler
 
-import "github.com/minguu42/tomeit/service"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/minguu42/tomeit/service"
+)
 
 // Handler -
 type Handler struct {
@@ -13,4 +16,16 @@ func New(svc service.Service) *Handler {
 	return &Handler{
 		svc: svc,
 	}
+}
+
+// Route はハンドラとエンドポイントを対応させる。
+func (h *Handler) Route(r chi.Router) {
+	r.Route("/tasks", func(r chi.Router) {
+		r.Post("/", h.CreateTask)
+		r.Get("/", h.ReadTask)
+		r.Patch("/{taskID}", h.UpdateTask)
+		r.Delete("/{taskID}", h.DeleteTask)
+	})
+
+	r.Get("/healthz", h.GetHealthz)
 }
