188a9f762e470944a8f8a6d065d6050e5c33bdf9
minguu42 <minguu42@gmail.com>
2022-07-24 18:28:22 +0900

    feat: error.goの追加

--- backend/pkg/error.go
+++ backend/pkg/error.go
@@ -11,6 +11,33 @@ type httpErrorResponse struct {
 	ErrorMessage string `json:"error"`
 }
 
+func newErrBadRequest(err error) *httpErrorResponse {
+	return &httpErrorResponse{
+		Error:        err,
+		StatusCode:   400,
+		Description:  "リクエストに何らかの誤りがあります。内容を確かめてからもう一度お試し下さい。",
+		ErrorMessage: err.Error(),
+	}
+}
+
+func newErrUnauthorized(err error) *httpErrorResponse {
+	return &httpErrorResponse{
+		Error:        err,
+		StatusCode:   401,
+		Description:  "ユーザの認証に失敗しました。もう一度お試し下さい。",
+		ErrorMessage: err.Error(),
+	}
+}
+
+func newErrNotFound(err error) *httpErrorResponse {
+	return &httpErrorResponse{
+		Error:        err,
+		StatusCode:   404,
+		Description:  "指定したリソースが存在しません。",
+		ErrorMessage: err.Error(),
+	}
+}
+
 func newErrInternalServerError(err error) *httpErrorResponse {
 	return &httpErrorResponse{
 		Error:        err,
