023133da11d530dff306ef6a8c9e14aa95eec229
minguu42 <minguu42@gmail.com>
2022-07-24 15:54:34 +0900

    feat!: healthz_*.goの追加

---
+++ backend/pkg/healthz_handler.go
@@ -0,0 +1,15 @@
+package tomeit
+
+import (
+	"net/http"
+
+	"github.com/minguu42/tomeit/pkg/logging"
+)
+
+// getHealthz は"GET /healthz"に対応するハンドラ
+func getHealthz(w http.ResponseWriter, _ *http.Request) {
+	if err := writeResponse(w, 200, &healthzResponse{Status: "OK"}); err != nil {
+		writeErrorResponse(w, newErrInternalServerError(err))
+		logging.Error("failed to write response.", err)
+	}
+}
