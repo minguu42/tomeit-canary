280a40bb4254a6e13bb6b7cd356ab70f4e62302a
minguu42 <minguu42@gmail.com>
2022-07-24 21:08:34 +0900

    feat: user_model.goの追加

---
+++ backend/pkg/user_model.go
@@ -0,0 +1,19 @@
+package tomeit
+
+import "time"
+
+type User struct {
+	ID        int       `db:"id" goqu:"skipinsert"`
+	DigestUID string    `db:"digest_uid"`
+	RestCount int       `db:"rest_count" goqu:"skipinsert"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+// HasTask はユーザがそのタスクを持っているかを判定する。
+func (u *User) HasTask(t *Task) bool {
+	if u.ID <= 0 || t.UserID <= 0 {
+		return false
+	}
+	return u.ID == t.UserID
+}
