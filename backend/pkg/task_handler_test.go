3dfebd771982f60e760c8a6ef94edfef97005917
minguu42 <minguu42@gmail.com>
2022-07-29 06:44:34 +0900

    feat: GetTasksの単体テストを追加

--- backend/pkg/task_handler_test.go
+++ backend/pkg/task_handler_test.go
@@ -121,3 +121,86 @@ func TestPostTasks(t *testing.T) {
 		}
 	}
 }
+
+func TestGetTasks(t *testing.T) {
+	SetDBOperator(&dbOperatorMock{})
+
+	type want struct {
+		statusCode int
+		response   interface{}
+	}
+	testcases := []struct {
+		name   string
+		target string
+		want   want
+	}{
+		{
+			name:   "タスク一覧を取得する",
+			target: "/tasks",
+			want: want{
+				statusCode: 200,
+				response: tasksResponse{Tasks: []*taskResponse{
+					{
+						ID:               1,
+						Title:            "数学の課題を終わらせる",
+						EstimatedPomoNum: 4,
+						CompletedPomoNum: 0,
+						DueOn:            "2021-07-10T00:00:00Z",
+						CompletedOn:      "",
+						CreatedAt:        time.Date(2021, 7, 9, 16, 30, 9, 0, time.UTC),
+						UpdatedAt:        time.Date(2021, 7, 9, 16, 30, 9, 0, time.UTC),
+					},
+					{
+						ID:               2,
+						Title:            "録画しておいた映画を観る",
+						EstimatedPomoNum: 0,
+						CompletedPomoNum: 0,
+						DueOn:            "",
+						CompletedOn:      "",
+						CreatedAt:        time.Date(2021, 7, 10, 21, 0, 49, 0, time.UTC),
+						UpdatedAt:        time.Date(2021, 7, 10, 21, 0, 49, 0, time.UTC),
+					},
+				}},
+			},
+		},
+		{
+			name:   "クエリパラメータisCompletedは真偽値である",
+			target: "/tasks?isCompleted=真",
+			want:   want{statusCode: 400},
+		},
+		{
+			name:   "クエリパラメータcompletedOnはRFC 3339 date-time形式である",
+			target: "/tasks?completedOn=2021-07-10",
+			want:   want{statusCode: 400},
+		},
+	}
+
+	for i, tc := range testcases {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", tc.target, nil)
+		r = r.WithContext(context.WithValue(r.Context(), userKey{},
+			&user{
+				ID:        1,
+				DigestUID: "a2c4ba85c41f186283948b1a54efacea04cb2d3f54a88d5826a7e6a917b28c5a",
+				RestCount: 0,
+				CreatedAt: time.Date(2021, 7, 9, 0, 0, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2021, 7, 9, 0, 0, 0, 0, time.UTC),
+			}))
+
+		GetTasks(w, r)
+
+		resp := w.Result()
+		if resp.StatusCode != tc.want.statusCode {
+			t.Errorf("#%d: Response status code should be %d, but %d", i+1, tc.want.statusCode, resp.StatusCode)
+		}
+		if resp.StatusCode == 200 {
+			var got tasksResponse
+			if err := json.NewDecoder(resp.Body).Decode(&got); err != nil {
+				t.Fatalf("#%d: failed to decode response body. %v", i+1, err)
+			}
+			if diff := cmp.Diff(tc.want.response, got); diff != "" {
+				t.Errorf("#%d: Response body is mismatch (-want +got):\n%s", i+1, diff)
+			}
+		}
+	}
+}
