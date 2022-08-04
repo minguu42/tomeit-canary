6c06bb23553532aa3bf239d561b0407c7c9bdcfb
minguu42 <minguu42@gmail.com>
2022-08-04 17:15:19 +0900

    fix: Authディレクトリを修正

--- src/components/features/Auth/index.tsx
+++ src/components/features/Auth/index.tsx
@@ -1,20 +1,2 @@
-import { FC } from "react";
-
-import Loading from "@/components/common/Loading";
-import {
-  useAccessControl,
-  useAuth,
-} from "@/components/features/Auth/Auth.hooks";
-
-type Props = {
-  children: JSX.Element;
-};
-
-const Auth: FC<Props> = ({ children }) => {
-  const isLoading = useAuth();
-  useAccessControl();
-
-  return isLoading ? <Loading /> : children;
-};
-
-export default Auth;
+export { default } from "./Auth";
+export { useAuth, useAccessControl } from "./Auth.hooks";
