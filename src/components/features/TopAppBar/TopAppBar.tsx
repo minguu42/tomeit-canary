8f81dbd795e458a55d03b78dd7bdefe24814124c
minguu42 <minguu42@gmail.com>
2022-08-07 16:50:37 +0900

    fix: TopAppBarの修正

--- src/components/features/TopAppBar/TopAppBar.tsx
+++ src/components/features/TopAppBar/TopAppBar.tsx
@@ -1,46 +1,37 @@
-import { FC } from "react";
-import Image from "next/image";
+import { FC, useState } from "react";
 
-import {
-  DarkModeIcon,
-  LightModeIcon,
-  LogoutIcon,
-} from "@/components/common/icons";
 import IconButton from "@/components/common/IconButton";
-import s from "./TopAppBar.module.css";
 import { useThemeActions } from "@/components/features/Theme";
+import { DarkModeIcon, LightModeIcon, MenuIcon } from "@/components/icons";
+import s from "./TopAppBar.module.css";
 import { useThemeAtom } from "@/globalStates/themeAtom";
-import { logout } from "@/lib/auth";
-import { imageLoader } from "@/lib/loader";
+import NavigationDrawer from "@/components/features/TopAppBar/NavigationDrawer";
 
+// TopAppBarの仕様はM3のSmall top app barに従う
+// https://m3.material.io/components/top-app-bar/specs
 const TopAppBar: FC = () => {
+  const [headline] = useState("Tomeit");
   const theme = useThemeAtom();
   const { toggleTheme } = useThemeActions();
+
   return (
-    <header className={s.container}>
-      <Image
-        src="icon.png"
-        width={30}
-        height={30}
-        alt=""
-        loader={imageLoader}
-        unoptimized
-      />
-      <h2 className={s.headline}>Tomeit</h2>
-      <div className={s.spacer} />
-      <IconButton
-        icon={theme === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
-        onClick={() => {
-          toggleTheme(theme);
-        }}
-        label="カラーテーマを切り替える"
-      />
-      <IconButton
-        icon={<LogoutIcon />}
-        onClick={() => void logout()}
-        label="ログアウトする"
-      />
-    </header>
+    <>
+      <header className={s.container}>
+        <IconButton
+          icon={<MenuIcon />}
+          label="ナビゲーションドロワーの切り替え"
+          onClick={() => console.log("a")}
+        />
+        <h2 className={s.headline}>{headline}</h2>
+        <div className={s.spacer} />
+        <IconButton
+          icon={theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+          onClick={() => toggleTheme(theme)}
+          label="カラーテーマを切り替え"
+        />
+      </header>
+      <NavigationDrawer activeIndicator="今日やること" />
+    </>
   );
 };
 
