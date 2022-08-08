8254322b7028a4edd982f3296e0170802953c236
minguu42 <minguu42@gmail.com>
2022-08-08 10:29:54 +0900

    feat: グローバルCSSの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
-import Theme from "@/components/features/Theme";
 import Auth from "@/components/features/Auth";
-import "@/pages/globals.css";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <RecoilRoot>
-      <Theme>
-        <Auth>
-          <Component {...pageProps} />
-        </Auth>
-      </Theme>
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
     </RecoilRoot>
   );
 }
---
+++ src/styles/globals.css.ts
@@ -0,0 +1,38 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("*", {
+  boxSizing: "border-box",
+  padding: "0",
+  margin: "0",
+  backgroundRepeat: "no-repeat",
+});
+
+globalStyle(":where(html)", {
+  fontFamily: `"Roboto", "Noto Sans JP", sans-serif`,
+});
+
+globalStyle(":where(a)", {
+  color: "inherit",
+  textDecoration: "none",
+});
+
+globalStyle(
+  `:where(button, [type="button"], [type="reset"], [type="submit"], [role="button"])`,
+  {
+    color: "inherit",
+    cursor: "pointer",
+    borderWidth: "0",
+  }
+);
+
+globalStyle(":where(nav, ol, ul, menu)", {
+  listStyleType: "none",
+});
+
+globalStyle(":where(svg:not([fill]))", {
+  fill: "currentcolor",
+});
+
+globalStyle(`:where([aria-hidden="false"], [disabled])`, {
+  cursor: "not-allowed",
+});
