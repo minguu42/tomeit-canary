c724ac91241d8e4bfad46ec49a7ac11709030530
minguu42 <minguu42@gmail.com>
2022-08-08 11:06:00 +0900

    feat: ステートレイヤートークンの追加

---
+++ src/styles/stateLayer.css.ts
@@ -0,0 +1,10 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+// ステートレイヤートークン
+// https://m3.material.io/foundations/interaction-states
+export const stateLayerOpacity = createGlobalTheme(":root", {
+  hover: "0.8",
+  focus: "0.12",
+  pressed: "0.12",
+  dragged: "0.16",
+});
