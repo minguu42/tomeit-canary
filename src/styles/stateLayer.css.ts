44be19459cac0041d55bf4580dd6952d04936ed9
minguu42 <minguu42@gmail.com>
2022-08-08 19:45:18 +0900

    fix: hover値の修正

--- src/styles/stateLayer.css.ts
+++ src/styles/stateLayer.css.ts
@@ -3,7 +3,7 @@ import { createGlobalTheme } from "@vanilla-extract/css";
 // ステートレイヤートークン
 // https://m3.material.io/foundations/interaction-states
 export const stateLayerOpacity = createGlobalTheme(":root", {
-  hover: "0.8",
+  hover: "0.08",
   focus: "0.12",
   pressed: "0.12",
   dragged: "0.16",
