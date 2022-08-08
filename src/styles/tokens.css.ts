2e6982b771f89b188ae0ff11460e01599f6572b2
minguu42 <minguu42@gmail.com>
2022-08-08 11:20:27 +0900

    feat: tokens.css.tsの追加

---
+++ src/styles/tokens.css.ts
@@ -0,0 +1,7 @@
+import { stateLayerOpacity } from "@/styles/stateLayer.css";
+import { theme } from "@/styles/theme.css";
+import { typography } from "@/styles/typography.css";
+
+const tokens = { stateLayerOpacity, ...theme, typography };
+
+export default tokens;
