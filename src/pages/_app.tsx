205df97fa5223ace30c11e7eb685694475ac7f93
minguu42 <minguu42@gmail.com>
2022-08-08 14:22:02 +0900

    feat: Themeの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
 import Auth from "@/components/features/Auth";
+import Theme from "@/components/features/Theme";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <RecoilRoot>
-      <Auth>
-        <Component {...pageProps} />
-      </Auth>
+      <Theme>
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      </Theme>
     </RecoilRoot>
   );
 }
