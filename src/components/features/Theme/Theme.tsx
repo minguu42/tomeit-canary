0ce82e5a471d6256b42814da0f1a9c5a6dcd02b9
minguu42 <minguu42@gmail.com>
2022-08-08 14:23:12 +0900

    fix: Themeの修正

--- src/components/features/Theme/Theme.tsx
+++ src/components/features/Theme/Theme.tsx
@@ -1,24 +1,15 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-import { useTheme } from "@/components/features/Theme";
+import { darkTheme, lightTheme } from "@/styles/theme.css";
 
 type Props = {
   children: JSX.Element;
 };
 
 const Theme: FC<Props> = ({ children }) => {
-  useTheme();
+  const [isDarkTheme] = useState(false);
 
-  return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `!function(){let e;const t=window.localStorage.getItem("theme");if(null!==t)e=t;else{e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",e)}();`,
-        }}
-      />
-      {children}
-    </>
-  );
+  return <div className={isDarkTheme ? darkTheme : lightTheme}>{children}</div>;
 };
 
 export default Theme;
