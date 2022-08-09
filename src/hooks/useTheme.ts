e24e914e37a674016f27267aa8f933f8fd19e7ed
minguu42 <minguu42@gmail.com>
2022-08-09 15:14:52 +0900

    fix: フォーマット

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,18 +3,18 @@ import { darkTheme, lightTheme } from "@/styles/tokens/theme.css";
 
 type UseTheme = {
   isDarkTheme: boolean;
-  toggleTheme: () => void
-}
+  toggleTheme: () => void;
+};
 
 export const useTheme = (): UseTheme => {
   const themeElement = window.document.getElementById("#theme");
-  const isDarkTheme = themeElement?.className === darkTheme
+  const isDarkTheme = themeElement?.className === darkTheme;
 
   const toggleTheme = useCallback(() => {
     const themeElement = window.document.getElementById("#theme");
     if (themeElement === null) return;
-    const isDarkTheme = themeElement.className === darkTheme
-    window.localStorage.setItem("theme", isDarkTheme ? "light" : "dark")
+    const isDarkTheme = themeElement.className === darkTheme;
+    window.localStorage.setItem("theme", isDarkTheme ? "light" : "dark");
     themeElement.className = isDarkTheme ? lightTheme : darkTheme;
   }, []);
 
