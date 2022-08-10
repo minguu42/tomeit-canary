3c439fbd6ba25e755d8c6b62c449c02d827ac516
minguu42 <minguu42@gmail.com>
2022-08-10 19:40:08 +0900

    fix: TopAppBarの修正

--- src/components/features/TopAppBar/TopAppBar.tsx
+++ src/components/features/TopAppBar/TopAppBar.tsx
@@ -1,18 +1,21 @@
 import { FC, useState } from "react";
 
+import FilledButton from "@/components/common/FilledButton";
 import IconButton from "@/components/common/IconButton";
 import {
   DarkModeIcon,
   LightModeIcon,
+  LoginIcon,
   MenuIcon,
   MenuOpenIcon,
 } from "@/components/icons";
 import * as s from "./TopAppBar.css";
-import { useTheme } from "@/hooks/useTheme";
+import { useIsLoginDialogOpenMutators } from "@/globalStates/isLoginDialogOpenAtom";
 import {
   useIsNavigationDrawerOpenAtom,
   useIsNavigationDrawerOpenMutators,
 } from "@/globalStates/isNavigationDrawerOpenAtom";
+import { useTheme } from "@/hooks/useTheme";
 
 // TopAppBarの仕様はM3のSmall top app barに従う
 // https://m3.material.io/components/top-app-bar/specs
@@ -21,6 +24,7 @@ const TopAppBar: FC = () => {
   const isNavigationDrawerOpen = useIsNavigationDrawerOpenAtom();
   const { toggleNavigationDrawer } = useIsNavigationDrawerOpenMutators();
   const { isDarkTheme, toggleTheme } = useTheme();
+  const { toggleLoginDialog } = useIsLoginDialogOpenMutators();
 
   return (
     <header className={s.container}>
@@ -31,6 +35,11 @@ const TopAppBar: FC = () => {
       />
       <h2 className={s.headline}>{headline}</h2>
       <div className={s.spacer} />
+      <FilledButton
+        icon={<LoginIcon />}
+        labelText="ログイン"
+        onClick={toggleLoginDialog}
+      />
       <IconButton
         icon={isDarkTheme ? <LightModeIcon /> : <DarkModeIcon />}
         label="テーマの切り替え"
