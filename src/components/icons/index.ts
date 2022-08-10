cb3e4cefc85db0f918ad75dfd84cb641e2ace65a
minguu42 <minguu42@gmail.com>
2022-08-10 20:35:59 +0900

    feat: Logoutアイコンの追加

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,5 +2,6 @@ export { default as AccountCircleIcon } from "./AccountCircle";
 export { default as DarkModeIcon } from "./DarkMode";
 export { default as LightModeIcon } from "./LightMode";
 export { default as LoginIcon } from "./Login";
+export { default as LogoutIcon } from "./Logout";
 export { default as MenuIcon } from "./Menu";
 export { default as MenuOpenIcon } from "./MenuOpen";
