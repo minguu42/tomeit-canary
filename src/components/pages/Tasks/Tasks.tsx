0780c7b73c4ad1451b92032c2132c8d3aafbda34
minguu42 <minguu42@gmail.com>
2022-08-11 15:27:09 +0900

    feat: TaskページにTaskListの追加

--- src/components/pages/Tasks/Tasks.tsx
+++ src/components/pages/Tasks/Tasks.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 
 import TopAppBar from "@/components/features/TopAppBar";
 import NavigationDrawer from "@/components/features/NavigationDrawer";
+import TaskList from "@/components/features/TaskList";
 import * as s from "./Tasks.css";
 
 // Tasksはタスク管理ページに対応するページコンポーネント
@@ -11,7 +12,9 @@ const Tasks: NextPage = () => {
       <TopAppBar />
       <div className={s.sideLayout}>
         <NavigationDrawer />
-        <div className={s.body}>body</div>
+        <div className={s.body}>
+            <TaskList />
+        </div>
       </div>
     </div>
   );
