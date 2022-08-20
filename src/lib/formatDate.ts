705bfe07abc770b1f3602d04d5160d17f50f536f
minguu42 <minguu42@gmail.com>
2022-08-20 20:24:06 +0900

    feat: formatDateの追加

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -6,14 +6,26 @@ export const formatSecondsToMinutesSeconds = (seconds: number): string => {
   return `${mm}:${ss}`;
 };
 
-export const formatDateToJP = (date: Date): string => {
-  return date.toLocaleDateString("ja-JP", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  });
-};
+type DateFormat = "RFC3339" | "locale";
+export const formatDate = (date: Date, format: DateFormat): string => {
+  if (format === "RFC3339") {
+    const yyyy = String(date.getFullYear());
+    const MM = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    const hh = String(date.getHours()).padStart(2, "0");
+    const mm = String(date.getMinutes()).padStart(2, "0");
+    const ss = String(date.getSeconds()).padStart(2, "0");
+    const now = new Date();
+    const timezoneOffsetInHours = now.getTimezoneOffset() / 60;
+    const symbol = timezoneOffsetInHours >= 0 ? "+" : "-";
+    const offset = String(Math.abs(timezoneOffsetInHours)).padStart(2, "0");
 
-export const formatDateStringToRFC3339 = (date: string): string => {
-  return date + "T00:00:00Z";
+    return `${yyyy}-${MM}-${dd}T${hh}:${mm}:${ss}${symbol}${offset}:00`;
+  } else {
+    return date.toLocaleDateString("ja-JP", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    });
+  }
 };
