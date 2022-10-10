1c8a4333b869f2053f92c7ef21ae11f333966099
minguu42 <minguu42@gmail.com>
2022-10-10 09:12:51 +0900

    fix: Themeの再実装

--- src/components/features/Theme/Theme.tsx
+++ src/components/features/Theme/Theme.tsx
@@ -1,21 +1,21 @@
 import { FC } from "react";
 
 import { darkTheme, lightTheme } from "@/styles/tokens/theme.css";
+import { useIsDarkTheme } from "@/globalStates/isDarkTheme";
+import { useTheme } from "./Theme.hooks";
 
 type Props = {
   children: JSX.Element;
 };
 
 const Theme: FC<Props> = ({ children }) => {
-  return (
-    <>
-      <div id="#theme">{children}</div>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `!function(){let e;const t=window.localStorage.getItem("theme");if(null!==t)e=t;else{e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.getElementById("#theme").className=e==="light"?"${lightTheme}":"${darkTheme}";console.log("once")}();`,
-        }}
-      />
-    </>
+  const isLoading = useTheme();
+  const isDarkTheme = useIsDarkTheme();
+
+  return isLoading ? (
+    <></>
+  ) : (
+    <div className={isDarkTheme ? darkTheme : lightTheme}>{children}</div>
   );
 };
 
