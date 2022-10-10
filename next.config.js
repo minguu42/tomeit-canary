d7e495305c35d8f687b9eb3b37b7b85248cf04d9
minguu42 <minguu42@gmail.com>
2022-10-10 17:26:00 +0900

    fix: ESLintの設定を単純なものに変更

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     loader: "custom",
   },
