8c40fb58c302e1eec3df8133010a2b94723d31a1
minguu42 <minguu42@gmail.com>
2022-10-13 19:29:17 +0900

    feat: TitleFieldを適用

--- src/components/features/SideSheet/SideSheet.tsx
+++ src/components/features/SideSheet/SideSheet.tsx
@@ -1,33 +1,58 @@
-import { FC } from "react";
+import { ChangeEventHandler, FC, useState } from "react";
 
-import { CircleIcon, TimerIcon } from "@/components/icons";
 import StandardIconButton from "@/components/common/StandardIconButton";
 import Content from "@/components/features/SideSheet/Content";
+import { CircleIcon, TimerIcon } from "@/components/icons";
 import * as s from "./SideSheet.css";
+import { useRemarkedTask } from "@/globalStates/remarkedTask";
+import { formatDate } from "@/lib/formatDate";
+import { Task } from "@/types/task";
+import TitleField from "@/components/features/SideSheet/TitleField";
 
-const SideSheet: FC = () => {
+type Props = {
+  task: Task;
+};
+
+const SideSheet: FC<Props> = () => {
+  const remarkedTask = useRemarkedTask();
+
+  if (remarkedTask === null) {
+    return <></>;
+  }
+
   return (
     <div className={s.container}>
       <div className={s.name}>
         <StandardIconButton
           icon={<CircleIcon />}
           label="完了"
-          onClick={() => window.alert("完了")}
+          onClick={() => window.alert(remarkedTask.title)}
         />
-        <h3>タスク1</h3>
+        <TitleField
+          taskID={remarkedTask.id}
+          initialTitle={remarkedTask.title}
+        />
       </div>
       <div className={s.divider} />
       <Content
         leadingIcon={<TimerIcon />}
         name="推定ポモドーロ数"
-        value={String(4)}
+        value={String(remarkedTask.estimatedCount)}
       />
       <Content
         leadingIcon={<TimerIcon />}
         name="実ポモドーロ数"
-        value={String(2)}
+        value={String(remarkedTask.actualCount)}
       />
-      <Content leadingIcon={<TimerIcon />} name="期限" value="2022-07-21" />
+      <Content
+        leadingIcon={<TimerIcon />}
+        name="期限"
+        value={
+          remarkedTask.dueOn !== null
+            ? formatDate(remarkedTask.dueOn, "locale")
+            : ""
+        }
+      />
     </div>
   );
 };
