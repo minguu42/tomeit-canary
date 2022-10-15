cbd9fd505971d78ee8d43fa6a03c3a22f0f83e97
minguu42 <minguu42@gmail.com>
2022-10-15 19:16:18 +0900

    feat: グローバルState playingTaskの追加

---
+++ src/globalStates/playingTask.ts
@@ -0,0 +1,34 @@
+import { Task } from "@/types/task";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { useCallback } from "react";
+
+type PlayingTask = Task | null;
+
+const playingTask = atom<PlayingTask>({
+  key: "playingTask",
+  default: null,
+});
+
+export const usePlayingTask = (): PlayingTask => useRecoilValue(playingTask);
+
+type PlayingTaskMutators = {
+  setPlayingTask: (task: Task) => void;
+  unsetPlayingTask: () => void;
+};
+
+export const usePlayingTaskMutators = (): PlayingTaskMutators => {
+  const setAtom = useSetRecoilState(playingTask);
+
+  const setPlayingTask = useCallback(
+    (task: Task) => {
+      setAtom(task);
+    },
+    [setAtom]
+  );
+
+  const unsetPlayingTask = useCallback(() => {
+    setAtom(null);
+  }, [setAtom]);
+
+  return { setPlayingTask, unsetPlayingTask };
+};
