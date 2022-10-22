b6613d4390ec151a0eac14597cc3248041844e7d
minguu42 <minguu42@gmail.com>
2022-10-22 19:47:57 +0900

    fix: DrawerにpaddingTopの追加

--- src/components/layouts/Layout/Drawer/Drawer.css.ts
+++ src/components/layouts/Layout/Drawer/Drawer.css.ts
@@ -3,6 +3,7 @@ import { style } from "@vanilla-extract/css";
 import { color } from "@/styles/tokens";
 
 export const container = style({
+  paddingTop: 8,
   width: "360px",
   backgroundColor: `rgb(${color.surface._})`,
   borderRadius: "0 16px 16px 0",
