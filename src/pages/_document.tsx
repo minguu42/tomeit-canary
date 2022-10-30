20addc79f0f22ec84a0ff705f00ceaeaa0a6a57f
minguu42 <minguu42@gmail.com>
2022-10-30 16:39:49 +0900

    fix: Google Fontの使用をやめた

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto:wght@400;500&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <body>
           <Main />
           <NextScript />
