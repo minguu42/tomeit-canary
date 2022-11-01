2c3534f0864a2849bed8c3fd03a3b274e3ebfb8c
minguu42 <minguu42@gmail.com>
2022-11-02 07:20:43 +0900

    fix: ページごとに表示するタスクを変更する

--- src/components/pages/Focus/Focus.tsx
+++ src/components/pages/Focus/Focus.tsx
@@ -2,11 +2,15 @@ import { NextPage } from "next";
 
 import TaskList from "@/components/features/TaskList";
 import * as s from "./Focus.css";
+import { useTasks } from "@/globalStates/tasks";
 
 const Focus: NextPage = () => {
+  const tasks = useTasks();
+  const filteredTasks = tasks.filter((t) => t.hasDoToday);
+
   return (
     <main className={s.main}>
-      <TaskList />
+      <TaskList tasks={filteredTasks} />
     </main>
   );
 };
