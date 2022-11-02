e19adb2c8a3a592e60383742fc173c428071e25a
FURUKAWA Akira <50062855+minguu42@users.noreply.github.com>
2022-11-03 08:52:06 +0900

    タスク完了の実装 (#162)
    * fix: サイドシートのTitleFieldのスタイル適用バグを修正
    
    * fix: ページごとに表示するタスクを変更する
    
    * fix: 完了したタスクは非表示にする
    
    * fix: RestをBreakに変更

--- src/components/features/TaskList/TaskList.tsx
+++ src/components/features/TaskList/TaskList.tsx
@@ -1,18 +1,18 @@
 import { FC } from "react";
 
 import TaskListItem from "@/components/features/TaskList/TaskListItem";
-import { useTasks } from "@/globalStates/tasks";
+import { Task } from "@/types/task";
 
-const TaskList: FC = () => {
-  let tasks = useTasks();
-
-  return (
-    <ul>
-      {tasks.map((task) => (
-        <TaskListItem task={task} key={task.id} />
-      ))}
-    </ul>
-  );
+type Props = {
+  tasks: Task[];
 };
+
+const TaskList: FC<Props> = ({ tasks }) => (
+  <ul>
+    {tasks.map((task) => (
+      <TaskListItem task={task} key={task.id} />
+    ))}
+  </ul>
+);
 
 export default TaskList;
--- src/components/pages/Tasks/Tasks.tsx
+++ src/components/pages/Tasks/Tasks.tsx
@@ -3,12 +3,16 @@ import { NextPage } from "next";
 import TaskAddForm from "@/components/features/TaskAddForm";
 import TaskList from "@/components/features/TaskList";
 import * as s from "./Tasks.css";
+import { useTasks } from "@/globalStates/tasks";
 
 const Tasks: NextPage = () => {
+  const tasks = useTasks();
+  const filteredTasks = tasks.filter((t) => t.completedOn === null);
+
   return (
     <main className={s.main}>
       <TaskAddForm />
-      <TaskList />
+      <TaskList tasks={filteredTasks} />
     </main>
   );
 };
