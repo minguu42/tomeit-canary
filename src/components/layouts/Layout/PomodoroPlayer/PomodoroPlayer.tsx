cdf38e2f7c8262793ed3fe8db9491118421b2a39
minguu42 <minguu42@gmail.com>
2022-11-03 08:49:32 +0900

    fix: RestをBreakに変更

--- src/components/layouts/Layout/PomodoroPlayer/PomodoroPlayer.tsx
+++ src/components/layouts/Layout/PomodoroPlayer/PomodoroPlayer.tsx
@@ -8,15 +8,15 @@ import { usePlayingTask } from "@/globalStates/playingTask";
 import Done from "@/components/common/icons/Done";
 
 const POMODORO_TIME = 1500;
-const SHORT_REST_TIME = 300;
-const LONG_REST_TIME = 900;
-const INIT_REST_COUNT = 4;
+const SHORT_BREAK_TIME = 300;
+const LONG_BREAK_TIME = 900;
+const INIT_BREAK_COUNT = 4;
 
 const PomodoroPlayer: FC = () => {
   const [time, setTime] = useState(POMODORO_TIME);
   const [isActive, setIsActive] = useState(false);
   const [isNextPomodoro, setIsNextPomodoro] = useState(false);
-  const [restCount, setRestCount] = useState(INIT_REST_COUNT);
+  const [breakCount, setBreakCount] = useState(INIT_BREAK_COUNT);
   const playingTask = usePlayingTask();
 
   useEffect(() => {
@@ -39,15 +39,15 @@ const PomodoroPlayer: FC = () => {
       setIsActive(false);
       if (isNextPomodoro) {
         setTime(POMODORO_TIME);
-        setRestCount((prev) => (prev === 1 ? 4 : prev - 1));
-      } else if (!isNextPomodoro && restCount !== 1) {
-        setTime(SHORT_REST_TIME);
-      } else if (!isNextPomodoro && restCount === 1) {
-        setTime(LONG_REST_TIME);
+        setBreakCount((prev) => (prev === 1 ? 4 : prev - 1));
+      } else if (!isNextPomodoro && breakCount !== 1) {
+        setTime(SHORT_BREAK_TIME);
+      } else if (!isNextPomodoro && breakCount === 1) {
+        setTime(LONG_BREAK_TIME);
       }
       setIsNextPomodoro((prev) => !prev);
     }
-  }, [isNextPomodoro, restCount, time]);
+  }, [isNextPomodoro, breakCount, time]);
 
   const startPomodoro = () => {
     setIsActive(true);
@@ -62,7 +62,7 @@ const PomodoroPlayer: FC = () => {
     setIsActive(false);
   };
 
-  const doneRest = () => {
+  const skipBreak = () => {
     setTime(0);
   };
 
@@ -74,7 +74,7 @@ const PomodoroPlayer: FC = () => {
     <div className={s.container}>
       {!isActive &&
         (time === POMODORO_TIME ||
-          ((time === SHORT_REST_TIME || time === LONG_REST_TIME) && isNextPomodoro)) && (
+          ((time === SHORT_BREAK_TIME || time === LONG_BREAK_TIME) && isNextPomodoro)) && (
           <IconButton icon={<PlayArrowIcon />} label="ポモドーロの開始" onClick={startPomodoro} />
         )}
       {!isActive && time !== POMODORO_TIME && !isNextPomodoro && (
@@ -87,7 +87,7 @@ const PomodoroPlayer: FC = () => {
         <IconButton icon={<PauseIcon />} label="ポモドーロの停止" onClick={pausePomodoro} />
       )}
       {isActive && isNextPomodoro && (
-        <IconButton icon={<Done />} label="休憩の完了" onClick={doneRest} />
+        <IconButton icon={<Done />} label="休憩の完了" onClick={skipBreak} />
       )}
       <h3 className={s.mgr16}>{formatSecondsToMinutesSeconds(time)}</h3>
       <p>{playingTask.title}</p>
