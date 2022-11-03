2d8d36e3071bd09e692011da07b3a8571561c0aa
minguu42 <minguu42@gmail.com>
2022-11-03 15:30:14 +0900

    feat: カレンダーを選択できないDatePickerの実装

---
+++ src/components/common/DatePicker/Menu/Menu.css.ts
@@ -0,0 +1,68 @@
+import { style } from "@vanilla-extract/css";
+
+import { color, typography } from "@/styles/tokens";
+
+export const container = style({
+  position: "absolute",
+  zIndex: 3,
+  top: 48,
+  left: 4,
+  width: 328,
+  height: 428,
+  borderRadius: 16,
+  backgroundColor: `rgb(${color.surface})`,
+});
+
+export const surfaceTint = style({
+  width: "100%",
+  height: "100%",
+  borderRadius: "inherit",
+  backgroundColor: `rgb(${color.primary} / 0.11)`, // surface3
+});
+
+export const header = style({
+  display: "flex",
+  alignItems: "center",
+  padding: "0 12px 0 16px",
+  height: 56,
+  color: `rgb(${color.on.surfaceVariant})`,
+});
+
+export const selector = style([
+  {
+    border: "0",
+    color: `rgb(${color.on.surfaceVariant})`,
+    backgroundColor: "transparent",
+  },
+  typography.label.large,
+]);
+
+export const spacer = style({
+  flex: "1 1 0",
+});
+
+export const calendarContainer = style({
+  padding: "0 12px",
+});
+
+export const weekdaysLabelContainer = style({
+  display: "flex",
+  gap: 4,
+});
+
+export const dateContainer = style({
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(40px, 1fr))",
+  gap: "0 4px",
+});
+
+export const calendarItem = style([
+  {
+    display: "grid",
+    placeItems: "center",
+    width: 40,
+    height: 40,
+    color: `rgb(${color.on.surface})`,
+  },
+  typography.body.large,
+]);
