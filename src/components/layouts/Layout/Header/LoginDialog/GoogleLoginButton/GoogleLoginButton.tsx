e76a00086a0605e670082e096899a8f1426718a9
minguu42 <minguu42@gmail.com>
2022-11-19 16:08:41 +0900

    fix: 画像をローカルパスでインポートする

--- src/components/layouts/Layout/Header/LoginDialog/GoogleLoginButton/GoogleLoginButton.tsx
+++ src/components/layouts/Layout/Header/LoginDialog/GoogleLoginButton/GoogleLoginButton.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Image from "next/image";
 
+import googleLogo from "../../../../../../../public/images/google.png"
 import * as s from "./GoogleLoginButton.css";
 
 type Props = {
@@ -12,7 +13,7 @@ export const GoogleLoginButton: FC<Props> = ({ login }) => (
     <div className={s.stateLayer} />
     <div className={s.logoBackground}>
       <Image
-        src="/images/google.png"
+        src={googleLogo}
         alt=""
         width={18}
         height={18}
