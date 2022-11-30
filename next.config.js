5b5dfedb81e950e0f4ea068b8574f378e58a37bb
minguu42 <minguu42@gmail.com>
2022-11-30 19:50:01 +0900

    fix: ESLintの削除

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    dirs: [
-      "src/components",
-      "src/globalStates",
-      "src/lib",
-      "src/pages",
-      "src/styles",
-      "src/types",
-    ],
-  },
 };
 
 module.exports = withVanillaExtract(nextConfig);
