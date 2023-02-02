715d835ab5ccefb3cf5a4099ab46ea4282523af2
minguu42 <minguu42@gmail.com>
2023-02-02 21:20:12 +0900

    feat: Themeコンポーネントの配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
+import { Theme } from "@/features/theme/Theme";
 import "@/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+    </RecoilRoot>
+  );
 };
 
 export default MyApp;
