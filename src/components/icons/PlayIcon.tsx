61d45df8313ee4e46272940aadb28a5ed5d28ed9
minguu42 <minguu42@gmail.com>
2023-02-03 19:12:51 +0900

    feat: いくつかのアイコンを追加する

---
+++ src/components/icons/PlayIcon.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  size?: 18 | 24;
+  className?: string;
+};
+
+export const PlayIcon = ({ size, className }: Props): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size ?? 24}
+    height={size ?? 24}
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M8 19V5l11 7Zm2-7Zm0 3.35L15.25 12 10 8.65Z" />
+  </svg>
+);
