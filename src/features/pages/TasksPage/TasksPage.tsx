a74265576a71989c2a90c5777fe88d3bda412eb5
minguu42 <minguu42@gmail.com>
2023-02-03 19:22:47 +0900

    feat: TasksPageを追加する

--- src/features/pages/TasksPage/TasksPage.tsx
+++ src/features/pages/TasksPage/TasksPage.tsx
@@ -1,7 +1,38 @@
+import { TaskAddForm } from "@/features/task/TaskAddForm/TaskAddForm";
+import { TaskList } from "@/features/task/TaskList/TaskList";
+import { Task } from "@/features/task/task";
+import * as s from "./TasksPage.css"
+
+const ts: Task[] = [
+  {
+    id: 1,
+    title: "タスク1",
+    estimatedCount: 4,
+    actualCount: 2,
+    dueOn: new Date(),
+    hasDoToday: true,
+    completedOn: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "タスク2",
+    estimatedCount: 0,
+    actualCount: 0,
+    dueOn: new Date(),
+    hasDoToday: false,
+    completedOn: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
 export const TasksPage = (): JSX.Element => {
   return (
-    <main>
-      <h1>Hello, World!</h1>
+    <main className={s.container}>
+      <TaskAddForm />
+      <TaskList tasks={ts} />
     </main>
   );
 };
