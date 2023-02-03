f98b6bc1fddb6b5ac95aa0c405f6a6cb099a4ab5
minguu42 <minguu42@gmail.com>
2023-02-03 15:33:15 +0900

    feat: Buttonを追加する

---
+++ src/components/Button/Button.css.ts
@@ -0,0 +1,78 @@
+import { style } from "@vanilla-extract/css";
+
+import { color, elevation, stateLayerOpacity, typography } from "@/styles/tokens.css";
+import { stateLayerBase } from "@/styles/utils.css";
+
+const containerBase = style({
+  position: "relative",
+  display: "grid",
+  placeItems: "center",
+  height: 40,
+  borderRadius: 20,
+  fontSize: typography.label.large.size,
+  fontWeight: typography.label.large.weight,
+});
+
+export const containerFilled = style([
+  containerBase,
+  {
+    padding: "0 24px",
+    color: color.on.primary,
+    backgroundColor: color.primary,
+    ":hover": {
+      zIndex: 1,
+      boxShadow: elevation.level1,
+    },
+    ":disabled": {
+      zIndex: "initial",
+      boxShadow: "none",
+      color: color.on.disabled,
+      backgroundColor: color.disabled,
+    },
+  },
+]);
+
+export const containerTonal = style([
+  containerBase,
+  {
+    padding: "0 24px",
+    color: color.on.primaryContainer,
+    backgroundColor: color.primaryContainer,
+    ":hover": {
+      zIndex: 1,
+      boxShadow: elevation.level1,
+    },
+    ":disabled": {
+      zIndex: "initial",
+      boxShadow: "none",
+      color: color.on.disabled,
+      backgroundColor: color.disabled,
+    },
+  },
+]);
+
+export const stateLayerFilled = style([
+  stateLayerBase,
+  {
+    backgroundColor: color.on.primary,
+    selectors: {
+      [`${containerFilled}:hover > &`]: { opacity: stateLayerOpacity.hover },
+      [`${containerFilled}:focus-visible > &`]: { opacity: stateLayerOpacity.focus },
+      [`${containerFilled}:active > &`]: { opacity: stateLayerOpacity.active },
+      [`${containerFilled}:disabled > &`]: { opacity: 0 },
+    },
+  },
+]);
+
+export const stateLayerTonal = style([
+  stateLayerBase,
+  {
+    backgroundColor: color.on.primaryContainer,
+    selectors: {
+      [`${containerTonal}:hover > &`]: { opacity: stateLayerOpacity.hover },
+      [`${containerTonal}:focus-visible > &`]: { opacity: stateLayerOpacity.focus },
+      [`${containerTonal}:active > &`]: { opacity: stateLayerOpacity.active },
+      [`${containerTonal}:disabled > &`]: { opacity: 0 },
+    },
+  },
+]);
