2c063ef5b095fd211d9cf522affa5cf623ddc1f2
minguu42 <minguu42@gmail.com>
2023-02-04 21:12:58 +0900

    feat: superjsonを適用する

--- src/hooks/useTRPC.ts
+++ src/hooks/useTRPC.ts
@@ -1,9 +1,11 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
 
 import type { AppRouter } from "@/server/routers/_app";
 
 export const useTRPC = () => {
   return createTRPCProxyClient<AppRouter>({
+    transformer: superjson,
     links: [httpBatchLink({ url: "/api/trpc" })],
   });
 };
