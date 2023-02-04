76f7a64ebb9a702cab92fba4579ba0e1e2a299aa
minguu42 <minguu42@gmail.com>
2023-02-04 21:14:14 +0900

    feat: task/listエンドポイントを追加する

--- src/features/pages/TasksPage/TasksPage.tsx
+++ src/features/pages/TasksPage/TasksPage.tsx
@@ -1,38 +1,20 @@
+import useSWR from "swr";
+
 import { TaskAddForm } from "@/features/task/TaskAddForm/TaskAddForm";
 import { TaskList } from "@/features/task/TaskList/TaskList";
-import { Task } from "@/features/task/task";
+import { useTRPC } from "@/hooks/useTRPC";
 import * as s from "./TasksPage.css";
 
-const ts: Task[] = [
-  {
-    id: 1,
-    title: "タスク1",
-    estimatedCount: 4,
-    actualCount: 2,
-    dueOn: new Date(),
-    hasDoToday: true,
-    completedOn: null,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 2,
-    title: "タスク2",
-    estimatedCount: 0,
-    actualCount: 0,
-    dueOn: new Date(),
-    hasDoToday: false,
-    completedOn: null,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
-
 export const TasksPage = (): JSX.Element => {
+  const trpc = useTRPC();
+  const { data } = useSWR("task/list", () => trpc.task.list.query());
+  if (!data) {
+    return <div>Loading...</div>;
+  }
   return (
     <main className={s.container}>
       <TaskAddForm />
-      <TaskList tasks={ts} />
+      <TaskList tasks={data.tasks} />
     </main>
   );
 };
--- src/server/routers/task.ts
+++ src/server/routers/task.ts
@@ -1,11 +1,34 @@
-import { z } from "zod";
-
+import type { Task } from "@/features/task/task";
 import { procedure, router } from "@/server/trpc";
 
 export const taskRouter = router({
-  hello: procedure.input(z.object({ text: z.string() })).query(({ input }) => {
+  list: procedure.query(() => {
+    const ts: Task[] = [
+      {
+        id: 1,
+        title: "タスク1",
+        estimatedCount: 4,
+        actualCount: 2,
+        dueOn: new Date(),
+        hasDoToday: true,
+        completedOn: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        title: "タスク2",
+        estimatedCount: 0,
+        actualCount: 0,
+        dueOn: new Date(),
+        hasDoToday: false,
+        completedOn: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
     return {
-      greeting: `hello ${input.text}`,
+      tasks: ts,
     };
   }),
 });
