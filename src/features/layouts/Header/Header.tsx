c61b6c010f84cfb26f3214b3f35fe5c2673a006b
minguu42 <minguu42@gmail.com>
2023-02-11 17:48:31 +0900

    fix: ログイン，ログアウトの処理を追加する

--- src/features/layouts/Header/Header.tsx
+++ src/features/layouts/Header/Header.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { IconButton } from "@/components/IconButton";
-import { DarkModeIcon, LightModeIcon, LogoutIcon } from "@/components/icons";
-import { useIsDarkTheme, useIsDarkThemeMutators } from "@/features/theme/useIsDarkTheme";
+import { signIn, signOut, useSession } from "next-auth/react";
 
+import { IconButton } from "@/components/IconButton";
+import { DarkModeIcon, LightModeIcon, LoginIcon, LogoutIcon } from "@/components/icons";
+import { useIsDarkTheme, useIsDarkThemeMutators } from "@/features/theme/useIsDarkTheme";
 import * as s from "./Header.css";
 
 export const Header: React.FC = () => {
+  const { status } = useSession();
   const isDarkTheme = useIsDarkTheme();
   const { toggleTheme } = useIsDarkThemeMutators();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
   return (
     <header className={s.container}>
       <div className={s.spacer} />
@@ -16,11 +23,11 @@ export const Header: React.FC = () => {
         label="UIテーマの切り替え"
         onClick={toggleTheme}
       />
-      <IconButton
-        icon={<LogoutIcon />}
-        label="ログアウト"
-        onClick={() => window.alert("ログアウト")}
-      />
+      {status === "unauthenticated" ? (
+        <IconButton icon={<LoginIcon />} label="ログイン" onClick={() => signIn()} />
+      ) : (
+        <IconButton icon={<LogoutIcon />} label="ログアウト" onClick={() => signOut()} />
+      )}
     </header>
   );
 };
